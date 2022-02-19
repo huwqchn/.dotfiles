424b3de68e4d1e5d5802b877d5c6632e73afeea1
huwq <huwqchn@gmail.com>
2022-02-20 03:46:57 +0800

    change oh_my_zsh to zim and complete zsh config

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias c='clear'
+alias ls="colorls"
+alias s="neofetch"
+alias lg='lazygit'
+alias ra='ranger'
+alias t='tmux'
+alias ta='tmux a'
+alias cdiff='colordiff'
---
+++ .config/zsh/env.zsh
@@ -0,0 +1,8 @@
+export XDG_CONFIG_HOME=$HOME/.config
+export TERM=xterm-256color
+export TERM_ITALICS=true
+export EDITOR=nvim
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+export TERM=screen-256color-bce
+export https_proxy=http://127.0.0.1:19180 http_proxy=http://127.0.0.1:19180 all_proxy=socks5://127.0.0.1:19180
