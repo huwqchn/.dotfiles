6746e2277220a2b4d73a1e85e38a661d398dbf86
huwq <huwqchn@gmail.com>
2022-02-20 17:27:15 +0800

    comment proxy env variables

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -5,4 +5,4 @@ export EDITOR=nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 export TERM=screen-256color-bce
-export https_proxy=http://127.0.0.1:19180 http_proxy=http://127.0.0.1:19180 all_proxy=socks5://127.0.0.1:19180
+# export https_proxy=http://127.0.0.1:19180 http_proxy=http://127.0.0.1:19180 all_proxy=socks5://127.0.0.1:19180
