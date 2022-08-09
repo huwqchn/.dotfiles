74f782440653ec4afe02d24b4dc69a2dd5f4eee0
Johnson <huwqchn@gmail.com>
2022-08-09 22:35:12 +0800

    add nvim lua +plugin which-key

--- .config/nvim-lua/lua/cores/plugins.lua
+++ .config/nvim-lua/lua/cores/plugins.lua
@@ -47,6 +47,8 @@ return requir('packer').startup(function(use)
   use 'folke/tokyonight.nvim'
   use 'lunarvim/darkplus.nvim'
 
+  -- which key
+  use { 'folke/which-key.nvim', event = "BufWinEnter" }
   -- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
