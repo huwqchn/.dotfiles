fbd040a26bc03a3bff67e64690906d969d252799
Johnson <huwqchn@gmail.com>
2022-08-27 15:23:34 +0800

    change lsp config

--- .config/nvim-lua/lua/config/lsp/lsp-installer.lua
+++ .config/nvim-lua/lua/config/lsp/lsp-installer.lua
@@ -25,22 +25,47 @@ lsp_installer.setup({
       server_pending = "➜",
       server_uninstalled = "✗",
     },
+    keymaps = {
+      -- Keymap to expand a server in the UI
+      toggle_server_expand = "<CR>",
+      -- Keymap to install the server under the current cursor position
+      install_server = "<SPACE>",
+      -- Keymap to reinstall/update the server under the current cursor position
+      update_server = "l",
+      -- Keymap to check for new version for the server under the current cursor position
+      check_server_version = "c",
+      -- Keymap to update all installed servers
+      update_all_servers = "L",
+      -- Keymap to check which installed servers are outdated
+      check_outdated_servers = "C",
+      -- Keymap to uninstall a server
+      uninstall_server = "X",
+    },
   },
-  keymaps = {
-    -- Keymap to expand a server in the UI
-    toggle_server_expand = "<CR>",
-    -- Keymap to install the server under the current cursor position
-    install_server = "<SPACE>",
-    -- Keymap to reinstall/update the server under the current cursor position
-    update_server = "l",
-    -- Keymap to check for new version for the server under the current cursor position
-    check_server_version = "c",
-    -- Keymap to update all installed servers
-    update_all_servers = "L",
-    -- Keymap to check which installed servers are outdated
-    check_outdated_servers = "C",
-    -- Keymap to uninstall a server
-    uninstall_server = "X",
+
+  pip = {
+    -- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
+    -- and is not recommended.
+    --
+    -- Example: { "--proxy", "https://proxyserver" }
+    install_args = {},
+  },
+
+  -- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
+  -- debugging issues with server installations.
+  log_level = vim.log.levels.INFO,
+
+  -- Limit for the maximum amount of servers to be installed at the same time. Once this limit is reached, any further
+  -- servers that are requested to be installed will be put in a queue.
+  max_concurrent_installers = 4,
+
+  github = {
+    -- The template URL to use when downloading assets from GitHub.
+    -- The placeholders are the following (in order):
+    -- 1. The repository (e.g. "rust-lang/rust-analyzer")
+    -- 2. The release version (e.g. "v0.3.0")
+    -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+    download_url_template = "https://github.com/%s/releases/download/%s/%s",
   },
 })
 
