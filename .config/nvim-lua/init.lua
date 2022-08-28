656a3b84c247819f8688c6693ededde0baa8b35b
Johnson <huwqchn@gmail.com>
2022-08-28 15:32:05 +0800

    change nvim config

--- .config/nvim-lua/init.lua
+++ .config/nvim-lua/init.lua
@@ -1,4 +1,1 @@
-require('core/plugins')
-require('core/keymaps')
-require('core/options')
-require('core/autocmd')
+require('core')
---
+++ .config/nvim-lua/lua/core/init.lua
@@ -0,0 +1,4 @@
+require('core/plugins')
+require('core/keymaps')
+require('core/options')
+require('core/autocmd')
