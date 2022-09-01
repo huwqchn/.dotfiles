f6139fdb4fdc25dc7899f177d036f41519a5e2a0
Johnson <huwqchn@gmail.com>
2022-09-01 22:35:03 +0800

    add vcpkg env path

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -7,4 +7,5 @@ export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 export TERM=screen-256color-bce
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="$HOME/.nix-profile/bin:$PATH"
+export PATH="$HOME/Github/vcpkg:$PATH"
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles/bottles
