444dcda2631162387d1f54dd0acfa03c8578ccca
Johnosn <huwqchn@gmail.com>
2022-09-06 11:50:57 +0800

    add export proxy

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,6 +8,10 @@ alias t='tmux'
 alias ta='tmux a'
 alias cdiff='colordiff'
 alias e='emacs -nw'
-alias ep='export https_proxy="127.0.0.1:19180" && export http_proxy="127.0.0.1:19180"'
+if [[ `uname` == 'Darwin' ]]; then
+	alias ep='export https_proxy=http://127.0.0.1:19180 && export http_proxy=http://127.0.0.1:19180'
+elif [[ `uname` == 'Linux' ]]; then
+	alias ep='export https_proxy=http://127.0.0.1:20171 && export http_proxy=http://127.0.0.1:20171'
+fi
 alias doom='~/.emacs.d/bin/doom'
 alias v='nvim'
