3154bd2fe8f4399451cbfb40a75515c5b5048359
Johnson <huwqchn@gmail.com>
2022-09-22 15:13:13 +0800

    change icon font, because I dont have regular hack nerd font

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -23,7 +23,7 @@ done
 
 sketchybar   --add item       separator left                          \
              --set separator  icon=                                  \
-                              icon.font="Hack Nerd Font:Regular:16.0" \
+                              icon.font="Hack Nerd Font:Bold:16.0" \
                               background.padding_left=26              \
                               background.padding_right=15             \
                               label.drawing=off                       \
