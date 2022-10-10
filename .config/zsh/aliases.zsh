7375601b0f55ca3a65702ce263fd970970c83234
arch johnson <huwqchn@gmail.com>
2022-10-10 11:51:39 +0800

    add some themes && change ep alias in linux

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,7 +10,7 @@ alias e='emacs -nw'
 if [[ `uname` == 'Darwin' ]]; then
 	alias ep='export https_proxy=http://127.0.0.1:19180 && export http_proxy=http://127.0.0.1:19180'
 elif [[ `uname` == 'Linux' ]]; then
-	alias ep='export https_proxy=http://127.0.0.1:20171 && export http_proxy=http://127.0.0.1:20171'
+	alias ep='export all_proxy=http://127.0.0.1:7890'
 fi
 alias doom='~/.emacs.d/bin/doom'
 alias v='nvim'
