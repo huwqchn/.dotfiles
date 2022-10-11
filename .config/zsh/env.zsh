f17e1847139b125d438d569f2eb4c9809239569f
arch johnson <huwqchn@gmail.com>
2022-10-12 00:35:44 +0800

    add some bspwm config

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -5,13 +5,21 @@ export EDITOR=nvim
 export ZSH_AUTOSUGGEST_USE_ASYNC=1
 export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 # export TERM=screen-256color-bce
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="$HOME/.nix-profile/bin:$PATH"
-export PATH="$HOME/.dotfiles/.bin:$PATH"
-export C_INCLUDE_PATH="/opt/homebrew/include:$C_INCLUDE_PATH"
-export CPLUS_INCLUDE_PATH="/opt/homebrew/include:$CPLUS_INCLUDE_PATH"
-export DYLD_LIBRARY_PATH="/usr/local/lib:$DYLD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="/opt/homebrew/lib:$DYLD_LIBRARY_PATH"
-export LIBRARY_PATH="/usr/local/lib:$LIBRARY_PATH"
-export LIBRARY_PATH="/opt/homebrew/lib:$LIBRARY_PATH"
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles/bottles
+
+if [[ `uname` == 'Darwin' ]]; then
+	export PATH="/opt/homebrew/bin:$PATH"
+	export PATH="$HOME/.nix-profile/bin:$PATH"
+	export PATH="$HOME/.dotfiles/.bin:$PATH"
+	export C_INCLUDE_PATH="/opt/homebrew/include:$C_INCLUDE_PATH"
+	export CPLUS_INCLUDE_PATH="/opt/homebrew/include:$CPLUS_INCLUDE_PATH"
+	export DYLD_LIBRARY_PATH="/usr/local/lib:$DYLD_LIBRARY_PATH"
+	export DYLD_LIBRARY_PATH="/opt/homebrew/lib:$DYLD_LIBRARY_PATH"
+	export LIBRARY_PATH="/usr/local/lib:$LIBRARY_PATH"
+	export LIBRARY_PATH="/opt/homebrew/lib:$LIBRARY_PATH"
+	export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles/bottles
+elif [[ `uname` == 'Linux' ]]; then
+	export GDK_SCALE=2
+	export GDK_DPI_SCALE=0.5
+	export QT_AUTO_SCREEN_SCALE_FACTOR=0
+	export QT_SCALE_FACTOR=2
+fi
