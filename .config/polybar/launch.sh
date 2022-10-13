63035572e7f26eb44d019bf06c4f16c04cc71285
arch johnson <huwqchn@gmail.com>
2022-10-13 17:14:41 +0800

    polybar config mv

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,57 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+## Files and Directories
+DIR="$HOME/.config/polybar"
+SFILE="$DIR/system.ini"
+RFILE="$DIR/.system"
+MFILE="$DIR/.module"
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+## Get system variable values for various modules
+get_values() {
+	CARD=$(basename "$(find /sys/class/backlight/* | head -n 1)")
+	BATTERY=$(basename "$(find /sys/class/power_supply/*BAT* | head -n 1)")
+	ADAPTER=$( "$(find /sys/class/power_supply/*AC* | head -n 1)")
+	INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
+}
 
-echo "Bars launched..."
+## Write values to `system` file
+set_values() {
+	if [[ "$ADAPTER" ]]; then
+		sed -i -e "s/adapter = .*/adapter = $ADAPTER/g" "$SFILE"
+	fi
+	if [[ "$BATTERY" ]]; then
+		sed -i -e "s/battery = .*/battery = $BATTERY/g" "$SFILE"
+	fi
+	if [[ "$CARD" ]]; then
+		sed -i -e "s/graphics_card = .*/graphics_card = $CARD/g" "$SFILE"
+	fi
+	if [[ "$INTERFACE" ]]; then
+		sed -i -e "s/network_interface = .*/network_interface = $INTERFACE/g" "$SFILE"
+	fi
+}
+
+## Launch Polybar with selected style
+launch_bar() {
+	CARD=$(basename "$(find /sys/class/backlight/* | head -n 1)")
+	if [[ "$CARD" != *"intel_"* ]]; then
+		if [[ ! -f "$MFILE" ]]; then
+			sed -i -e 's/backlight/brightness/g' "$DIR"/config
+			touch "$MFILE"
+		fi
+	fi
+
+	if [[ ! $(pidof polybar) ]]; then
+		polybar -q bar -c "$DIR"/config &
+	else
+		polybar-msg cmd restart
+	fi
+}
+
+# Execute functions
+if [[ ! -f "$RFILE" ]]; then
+	get_values
+	set_values
+	touch "$RFILE"
+fi
+
+launch_bar
