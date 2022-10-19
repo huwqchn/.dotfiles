40b0cef483d2d42d71578c68571d107fdcccc0fb
arch johnson <huwqchn@gmail.com>
2022-10-19 17:44:57 +0800

    change clash running color

--- .config/polybar/scripts/clash.sh
+++ .config/polybar/scripts/clash.sh
@@ -6,5 +6,5 @@ if [ ! -z "$(systemctl status docker | grep inactive)" ]; then
 elif [ -z "$(docker ps -a | grep clash | grep Up)" ]; then
 	echo "%{F#e0af68}  "
 else
-	echo "%{F#7aa2f7}  "
+	echo "%{F#9ece6a}  "
 fi
