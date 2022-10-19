6f73b0f78cf30b7348443c3f043c7bf353d986f8
arch johnson <huwqchn@gmail.com>
2022-10-19 17:42:40 +0800

    fixed clash script bug

--- .config/polybar/scripts/clash.sh
+++ .config/polybar/scripts/clash.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CLASH=$(docker ps -a | grep clash | grep Up)
 
-if [ -z "$CLASH" ]; then
-	# echo "%{F}  "
+if [ ! -z "$(systemctl status docker | grep inactive)" ]; then
+	echo "%{F}  "
+elif [ -z "$(docker ps -a | grep clash | grep Up)" ]; then
 	echo "%{F#e0af68}  "
 else
 	echo "%{F#7aa2f7}  "
