fe1e3b22c944e075351f568c62328d47dfd99e0e
arch johnson <huwqchn@gmail.com>
2022-11-16 00:07:50 +0800

    add a comment in zsh env

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -24,6 +24,7 @@ if [[ $(uname) == 'Darwin' ]]; then
 elif [[ $(uname) == 'Linux' ]]; then
   export TERMINAL="alacritty"
   export PATH=$PATH:/snap/bin
+  # for hidpi screen
 	export GDK_SCALE=2
 	export GDK_DPI_SCALE=0.5
 	export QT_AUTO_SCREEN_SCALE_FACTOR=0
