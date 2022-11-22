194b6207e26367d0b0aba25441a4f2b7f4e8ae60
arch johnson <huwqchn@gmail.com>
2022-11-22 13:32:45 +0800

    set rofi drun dpi = 1

--- .scripts/applauncher.sh
+++ .scripts/applauncher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -show drun -icon-theme "Papirus" -show-icons -hover-select -me-select-entry '' -me-accept-entry MousePrimary
+rofi -show drun -icon-theme "Papirus" -show-icons -hover-select -me-select-entry '' -me-accept-entry MousePrimary -dpi 1
