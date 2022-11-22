f11843c8ee4dd0b9bc7f2459a7005a8e528f1f38
arch johnson <huwqchn@gmail.com>
2022-11-22 13:04:38 +0800

    ncmpcpp zsh ui theme config

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -30,3 +30,25 @@ elif [[ $(uname) == 'Linux' ]]; then
 	export QT_AUTO_SCREEN_SCALE_FACTOR=0
 	export QT_SCALE_FACTOR=1
 fi
+
+# FZF bases
+export FZF_DEFAULT_OPTS="
+  --color fg:$color7
+  --color fg+:$color0
+  --color bg:$background
+  --color bg+:$color0
+  --color hl:$color10
+  --color hl+:$color2
+  --color info:$color4
+  --color prompt:$color4
+  --color spinner:$color12
+  --color pointer:$color7
+  --color marker:$color5
+  --color border:$background
+  --color gutter:$color0
+  --color header:$color8
+  --prompt ' '
+  --pointer ' λ'
+  --layout=reverse
+  --border horizontal
+  --height 40"
