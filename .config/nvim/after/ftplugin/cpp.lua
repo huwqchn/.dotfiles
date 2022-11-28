280d3f47d61a6dc311cd5eefc70bc520712218a1
arch johnson <huwqchn@gmail.com>
2022-11-28 18:52:42 +0800

    rm annoy warning

---
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,8 @@
+local notify = vim.notify
+vim.notify = function(msg, ...)
+    if msg:match("warning: multiple different client offset_encodings") then
+        return
+    end
+
+    notify(msg, ...)
+end
