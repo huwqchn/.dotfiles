a7bdb20c9dd0656953f4bbb18a63c1639ee3f77f
arch johnson <huwqchn@gmail.com>
2022-11-28 18:55:42 +0800

    add cpp lsp supports

--- .config/nvim/lua/saturn/plugins/extra/lsp/languages/cpp.lua
+++ .config/nvim/lua/saturn/plugins/extra/lsp/languages/cpp.lua
@@ -1,2 +1,5 @@
   local manager = require 'saturn.plugins.core.lsp.manager'
-  manager.setup("clangd", {} )
+  manager.setup("clangd", {
+    on_init = require("saturn.plugins.core.lsp").common_on_init,
+    capabilities = require("saturn.plugins.core.lsp").common_capabilities(),
+  } )
