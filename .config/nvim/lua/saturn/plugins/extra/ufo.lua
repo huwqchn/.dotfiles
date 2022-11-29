db87909ccac3c39e8ee12e9a6fd450afc54fe6f8
arch johnson <huwqchn@gmail.com>
2022-11-29 18:52:45 +0800

    rm autocmd

--- .config/nvim/lua/saturn/plugins/extra/ufo.lua
+++ .config/nvim/lua/saturn/plugins/extra/ufo.lua
@@ -2,12 +2,12 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-vim.cmd(
-[[
-  au BufWinLeave * silent mkview
-  au BufWinEnter * silent loadview
-]]
-)
+-- vim.cmd(
+-- [[
+--   au BufWinLeave * silent mkview
+--   au BufWinEnter * silent loadview
+-- ]]
+-- )
 
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
