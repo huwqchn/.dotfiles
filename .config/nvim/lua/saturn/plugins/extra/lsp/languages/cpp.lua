fb0ef4b26185a37a6c7e5df016abbfcca25b6a8d
arch johnson <huwqchn@gmail.com>
2022-11-30 12:03:31 +0800

    comment linter to noisy

--- .config/nvim/lua/saturn/plugins/extra/lsp/languages/cpp.lua
+++ .config/nvim/lua/saturn/plugins/extra/lsp/languages/cpp.lua
@@ -1,5 +1,12 @@
-  local manager = require 'saturn.plugins.core.lsp.manager'
-  manager.setup("clangd", {
-    on_init = require("saturn.plugins.core.lsp").common_on_init,
-    capabilities = require("saturn.plugins.core.lsp").common_capabilities(),
-  } )
+-- set a linter for cpp
+-- local linters = require "saturn.plugins.core.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "cpplint", filetypes = { "cpp" } }
+-- }
+
+-- set a lsp for cpp
+local manager = require 'saturn.plugins.core.lsp.manager'
+manager.setup("clangd", {
+  on_init = require("saturn.plugins.core.lsp").common_on_init,
+  capabilities = require("saturn.plugins.core.lsp").common_capabilities(),
+} )
