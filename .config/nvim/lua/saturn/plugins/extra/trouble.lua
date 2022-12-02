8ae1845151a1662e4ab1a8d6f7ce9a9b95a3c883
arch johnson <huwqchn@gmail.com>
2022-12-02 14:49:37 +0800

    trouble plugins keymaps changes

--- .config/nvim/lua/saturn/plugins/extra/trouble.lua
+++ .config/nvim/lua/saturn/plugins/extra/trouble.lua
@@ -50,17 +50,15 @@ M.config = function()
     },
     use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
   }
-  if saturn.plugins.trouble.active then
-    saturn.plugins.whichkey.mappings["D"] = {
-      name = "Diagnostics",
-      t = { "<cmd>TroubleToggle<cr>", "trouble" },
-      w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-      d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
-      q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-      l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-      r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-    }
-  end
+  saturn.plugins.whichkey.mappings["D"] = {
+    name = "Diagnostics",
+    t = { "<cmd>TroubleToggle<cr>", "trouble" },
+    w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+    d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+    q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+    l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+    r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+  }
 end
 
 M.setup = function()
