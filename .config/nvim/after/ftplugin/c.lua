e6331c0ba9f1335678043ba62e1b9aa1e5a9510c
arch johnson <huwqchn@gmail.com>
2022-12-08 11:50:52 +0800

    nvim aftre c filetype

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -7,3 +7,14 @@ vim.notify = function(msg, ...)
     notify(msg, ...)
 end
 
+vim.keymap.set('n', "<space><space>", ":e %:p:s,.h$,.X123X,:s,.cpp$,.h,:s,.X123X$,.c,<CR>", { noremap = true, silent = true } )
+
+vim.bo.textwidth   = 100
+vim.bo.tabstop     = 4
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth  = 4
+
+vim.bo.commentstring = '// %s'
+
+vim.bo.cindent = true -- stricter rules for C programs
+vim.opt.cinoptions:append('g0')
