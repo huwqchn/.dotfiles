2e9de42e68550589bf70cecd454adf322c3a32a2
arch johnson <huwqchn@gmail.com>
2022-12-11 17:03:22 +0800

    fix time script

--- .config/polybar/scripts/time.sh
+++ .config/polybar/scripts/time.sh
@@ -13,42 +13,39 @@ toggle() {
 
 trap "toggle" USR1
 
-#Get the current time
-time=$(date "+%I:%M %p")
-time_alt=$(date "+%A, %d %B %Y")
-# Get the hour of the day
-hour=$(date +"%I")
-
-# Set the clock icon to use
-if [ "$hour" -eq 1 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 2 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 3 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 4 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 5 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 6 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 7 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 8 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 9 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 10 ]; then
-  clock_icon="%{T3} %{T-}"
-elif [ "$hour" -eq 11 ]; then
-  clock_icon="%{T3} %{T-}"
-else
-  clock_icon="%{T3} %{T-}"
-fi
-
-# Output the clock icon and the current time
-echo -e "$clock_icon$time"
 while true; do
+  #Get the current time
+  time=$(date "+%I:%M %p")
+  time_alt=$(date "+%A, %d %B %Y")
+  # Get the hour of the day
+  hour=$(date +"%I")
+
+  # Set the clock icon to use
+  if [ "$hour" -eq 1 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 2 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 3 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 4 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 5 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 6 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 7 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 8 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 9 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 10 ]; then
+    clock_icon="%{T3} %{T-}"
+  elif [ "$hour" -eq 11 ]; then
+    clock_icon="%{T3} %{T-}"
+  else
+    clock_icon="%{T3} %{T-}"
+  fi
   if [ $t -eq 0 ]; then
     echo -e "$clock_icon$time"
   else
