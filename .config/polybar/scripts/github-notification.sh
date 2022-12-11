b0313513d4b2eed4cd5f30f6a402928eeed6c016
arch johnson <huwqchn@gmail.com>
2022-12-11 19:03:05 +0800

    github-token

--- .config/polybar/scripts/github-notification.sh
+++ .config/polybar/scripts/github-notification.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 USER="huwqchn@gmail.com"
 # You can get your Personal access tokens from here : https://github.com/settings/tokens #
-TOKEN=$(pass online/github-token)
+TOKEN=$(cat ~/Documents/github-token)
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
