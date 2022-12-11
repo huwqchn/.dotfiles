7b606c4b100848de9443e27b8a6658d7f15c79a0
arch johnson <huwqchn@gmail.com>
2022-12-11 16:07:56 +0800

    github icon change

--- .config/polybar/scripts/github-notification.sh
+++ .config/polybar/scripts/github-notification.sh
@@ -6,6 +6,7 @@ TOKEN=$(pass online/github-token)
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 if [ "$notifications" -gt 0 ]; then
+  # echo "%{T3} $notifications"
   echo "$notifications"
 fi
 
