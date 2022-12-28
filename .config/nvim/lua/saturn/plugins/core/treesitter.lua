da36a66926d9fe53b15b79a9e10478ff26d25ce9
arch johnson <huwqchn@gmail.com>
2022-12-28 13:19:11 +0800

    add zk markdown wikilinks syntax supports

--- .config/nvim/lua/saturn/plugins/core/treesitter.lua
+++ .config/nvim/lua/saturn/plugins/core/treesitter.lua
@@ -22,7 +22,7 @@ function M.config()
     },
     highlight = {
       enable = true, -- false will disable the whole extension
-      additional_vim_regex_highlighting = false,
+      additional_vim_regex_highlighting = { "markdown" },
       disable = function(lang, buf)
         if vim.tbl_contains({ "latex" }, lang) then
           return true
