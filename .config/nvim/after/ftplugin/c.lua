a00053794efab16a1965e06e215b6342bbbd1acf
arch johnson <huwqchn@gmail.com>
2022-12-30 11:55:07 +0800

    set no wrap in c/cpp file

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -17,4 +17,4 @@ vim.opt_local.commentstring = '// %s'
 
 vim.opt_local.cindent = true -- stricter rules for C programs
 vim.opt_local.cinoptions:append('g0,N-s,E-s,l1,:0')
-vim.wo.warp = false
+vim.wo.wrap = false
--- .config/nvim/after/ftplugin/cpp.lua
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -18,7 +18,7 @@ vim.opt_local.commentstring = '// %s'
 
 vim.opt_local.cindent = true -- stricter rules for C programs
 vim.opt_local.cinoptions:append('g0,N-s,E-s,l1,:0')
-vim.wo.warp = false
+vim.wo.wrap = false
 
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
