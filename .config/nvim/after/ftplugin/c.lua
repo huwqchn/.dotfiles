16bc9a716b8a0519bb4b701c985516618d738773
arch johnson <huwqchn@gmail.com>
2022-12-30 11:35:27 +0800

    c/cpp indent options

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -16,4 +16,5 @@ vim.opt_local.shiftwidth  = 2
 vim.opt_local.commentstring = '// %s'
 
 vim.opt_local.cindent = true -- stricter rules for C programs
-vim.opt_local.cinoptions:append('g0')
+vim.opt_local.cinoptions:append('g0,N-s,E-s,l1,:0')
+vim.wo.warp = false
--- .config/nvim/after/ftplugin/cpp.lua
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -17,7 +17,8 @@ vim.opt_local.shiftwidth  = 2
 vim.opt_local.commentstring = '// %s'
 
 vim.opt_local.cindent = true -- stricter rules for C programs
-vim.opt_local.cinoptions:append('g0')
+vim.opt_local.cinoptions:append('g0,N-s,E-s,l1,:0')
+vim.wo.warp = false
 
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
