27efbc6541d0eae6e1d4b7e0493e17e3777e6b67
arch johnson <huwqchn@gmail.com>
2023-01-03 13:19:26 +0800

    debug

--- .config/nvim/lua/saturn/plugins/init.lua
+++ .config/nvim/lua/saturn/plugins/init.lua
@@ -5,12 +5,12 @@ local core = require 'saturn.plugins.core'
 core.config()
 
 local extra = require 'saturn.plugins.extra'
-extra.config()
+-- extra.config()
 
 plugin_loader.load( { core.get(), saturn.enable_extra_plugins and extra.get() or {} } )
 
 core.setup()
-extra.setup()
+-- extra.setup()
 
 require('saturn.plugins.extra.lsp').setup()
 require('saturn.plugins.core.lsp').setup()
