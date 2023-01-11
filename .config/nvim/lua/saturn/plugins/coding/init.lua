85a58ffd8610aa715ef0d83c0730e7b195532945
arch johnson <huwqchn@gmail.com>
2023-01-11 12:29:36 +0800

    change tabnine config

--- .config/nvim/lua/saturn/plugins/coding/init.lua
+++ .config/nvim/lua/saturn/plugins/coding/init.lua
@@ -28,9 +28,20 @@ return {
       {
         "tzachar/cmp-tabnine",
         build = "./install.sh",
-        config = {
-          show_prediction_strength = true,
-        }
+        config = function()
+          require("cmp_tabnine.config").setup({
+            max_lines = 1000,
+            max_num_results = 20,
+            sort = true,
+            run_on_every_keystroke = true,
+            snippet_placeholder = "..",
+            ignored_file_types = { -- default is not to ignore
+              -- uncomment to ignore in lua:
+              -- lua = true
+            },
+            show_prediction_strength = true,
+          })
+        end,
       },
     },
     config = function()
