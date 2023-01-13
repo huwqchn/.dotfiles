0e527a8ae6774914911be677b8fa20532393dc34
arch johnson <huwqchn@gmail.com>
2023-01-13 16:13:19 +0800

    try noice again

--- .config/nvim/lua/saturn/plugins/ui/init.lua
+++ .config/nvim/lua/saturn/plugins/ui/init.lua
@@ -221,34 +221,107 @@ return {
   },
 
   -- noicer ui too lag and noise
-  -- {
-  --   "folke/noice.nvim",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     lsp = {
-  --       override = {
-  --         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --         ["vim.lsp.util.stylize_markdown"] = true,
-  --       },
-  --       -- signature = {
-  --       --   enabled = false,
-  --       -- }
-  --     },
-  --     presets = {
-  --       bottom_search = true,
-  --       command_palette = true,
-  --       long_message_to_split = true,
-  --     },
-  --   },
-  --   -- stylua: ignore
-  --   keys = {
-  --     { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-  --     { "<leader>ul", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-  --     { "<leader>uh", function() require("noice").cmd("history") end, desc = "Noice History" },
-  --     { "<leader>ua", function() require("noice").cmd("all") end, desc = "Noice All" },
-  --     { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward" },
-  --     { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward"},
-  --   },
-  -- },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+    local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      require("noice").setup({
+        debug = false,
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        signature = {
+          enabled = false,
+        },
+        routes = {
+          {
+            filter = {
+              cond = function()
+                return not focused
+              end,
+            },
+            view = "notify_send",
+            opts = { stop = false },
+          },
+          {
+            filter = {
+              event = "msg_show",
+              find = "%d+L, %d+B",
+            },
+            view = "mini",
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = true,
+          cmdline_output_to_split = false,
+        },
+        commands = {
+          all = {
+            -- options for the message history that you get with `:Noice`
+            view = "split",
+            opts = { enter = true, format = "details" },
+            filter = {},
+          },
+        },
+        format = {
+          level = {
+            icons = false,
+          },
+        },
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+    end,
+    -- opts = {
+    --   lsp = {
+    --     override = {
+    --       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+    --       ["vim.lsp.util.stylize_markdown"] = true,
+    --     },
+    --     -- signature = {
+    --     --   enabled = false,
+    --     -- }
+    --   },
+    --   presets = {
+    --     bottom_search = true,
+    --     command_palette = true,
+    --     long_message_to_split = true,
+    --   },
+    -- },
+    -- stylua: ignore
+    keys = {
+      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
+      { "<leader>ul", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+      { "<leader>uh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>ua", function() require("noice").cmd("all") end, desc = "Noice All" },
+      { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward" },
+      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward"},
+    },
+  },
 
 }
