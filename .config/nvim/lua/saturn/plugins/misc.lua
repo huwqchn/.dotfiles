2e618a782b5ba2b61b8cd1e8c99bb249d05afbc2
arch johnson <huwqchn@gmail.com>
2023-01-15 03:32:33 +0800

    rm persistence

--- .config/nvim/lua/saturn/plugins/misc.lua
+++ .config/nvim/lua/saturn/plugins/misc.lua
@@ -147,18 +147,7 @@ return {
     config = true,
 
   },
-  -- session management
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
-    -- stylua: ignore
-    keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-    },
-  },
+  --TODO:session management
   {
     "NvChad/nvim-colorizer.lua",
     event = "BufRead",
