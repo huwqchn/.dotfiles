9b7335e0afac692707b27fa6319c50ca333d1028
arch johnson <huwqchn@gmail.com>
2023-01-27 03:20:40 +0800

    use zk-nvim

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,57 @@
 vim.o.wrap = true
+
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
+
+local opts = {
+  mode = "n", -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = true, -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+  n = {
+    name = "Note",
+  },
+}
+
+which_key.register(mappings, opts)
+
+-- Add the key mappings only for Markdown files in a zk notebook.
+if require("zk.util").notebook_root(vim.fn.expand("%:p")) ~= nil then
+  local function map(...)
+    vim.api.nvim_buf_set_keymap(0, ...)
+  end
+  opts = { noremap = true, silent = false }
+
+  -- Open the link under the caret.
+  map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  -- Preview a linked note.
+  map("n", "H", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  -- Create a new note after asking for its title.
+  -- This overrides the global `<leader>zn` mapping to create the note in the same directory as the current buffer.
+  map("n", "<leader>nn", "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>", opts)
+  -- Create a new note in the same directory as the current buffer, using the current selection for title.
+  map("v", "<leader>nt", ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>", opts)
+  -- Create a new note in the same directory as the current buffer, using the current selection for note content and asking for its title.
+  map(
+    "v",
+    "<leader>nc",
+    ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
+    opts
+  )
+
+  -- Open notes linking to the current buffer.
+  map("n", "<leader>nb", "<Cmd>ZkBacklinks<CR>", opts)
+  -- Alternative for backlinks using pure LSP and showing the source context.
+  --map('n', '<leader>zb', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+  -- Open notes linked by the current buffer.
+  map("n", "<leader>nl", "<Cmd>ZkLinks<CR>", opts)
+  -- Open the code actions for a visual selection.
+  map("v", "<leader>na", ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", opts)
+end
