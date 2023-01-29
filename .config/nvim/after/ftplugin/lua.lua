b03211227cf4a02692b087856793ee7ecfd15b68
arch johnson <huwqchn@gmail.com>
2023-01-29 16:10:54 +0800

    comment dap too slow load

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -3,27 +3,26 @@ require("saturn.plugins.lsp.null-ls.formatters").setup({
   { command = "stylua", args = { "lua" } },
 })
 
-local ok, dap = pcall(require, "dap")
-if not ok then
-  return
-end
+-- local ok, dap = pcall(require, "dap")
+-- if not ok then
+--   return
+-- end
 
-dap.configurations.lua = {
-  {
-    type = "nlua",
-    request = "attach",
-    name = "Attach to running Neovim instance",
-  },
-}
-dap.adapters.nlua = function(callback, config)
-  callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
-end
+-- dap.configurations.lua = {
+--   {
+--     type = "nlua",
+--     request = "attach",
+--     name = "Attach to running Neovim instance",
+--   },
+-- }
+-- dap.adapters.nlua = function(callback, config)
+--   callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+-- end
 
-vim.keymap.set("n", "<leader>ds", function()
-  require("osv").launch({ port = 8086 })
-end, { desc = "Launch Lua Debugger Server" })
+-- vim.keymap.set("n", "<leader>ds", function()
+--   require("osv").launch({ port = 8086 })
+-- end, { desc = "Launch Lua Debugger Server" })
 
-vim.keymap.set("n", "<leader>dd", function()
-  require("osv").run_this()
-end, { desc = "Launch Lua Debugger" })
-
+-- vim.keymap.set("n", "<leader>dd", function()
+--   require("osv").run_this()
+-- end, { desc = "Launch Lua Debugger" })
