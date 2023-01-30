759fc5cb0c29334c6bafdfbc7a9899a251af4425
arch johnson <huwqchn@gmail.com>
2023-01-30 15:11:32 +0800

    rust config

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -1,32 +1,3 @@
-local ok, dap = pcall(require, "dap")
-if not ok then
-  return
-end
-
-local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
-local codelldb_adapter = {
-  type = "server",
-  port = "${port}",
-  executable = {
-    command = mason_path .. "bin/codelldb",
-    args = { "--port", "${port}" },
-  },
-}
-
-dap.adapters.codelldb = codelldb_adapter
-dap.configurations.rust = {
-  {
-    name = "Launch file",
-    type = "codelldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-  },
-}
-
 local status_ok, which_key = pcall(require, "which-key")
 if not status_ok then
   return
