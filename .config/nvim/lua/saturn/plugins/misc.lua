129afddd29e7f2bf06345202abf214cfa5602449
arch johnson <huwqchn@gmail.com>
2023-02-03 16:45:30 +0800

    add session manager plugin

--- .config/nvim/lua/saturn/plugins/misc.lua
+++ .config/nvim/lua/saturn/plugins/misc.lua
@@ -310,4 +310,17 @@ return {
       require("color-picker")
     end,
   },
+
+  -- session management
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+    -- stylua: ignore
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
 }
