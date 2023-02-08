96ffe6c74d494be45d7385aaea17ce0b5b407952
arch johnson <huwqchn@gmail.com>
2023-02-08 22:27:04 +0800

    color-picker config

--- .config/nvim/lua/saturn/plugins/misc.lua
+++ .config/nvim/lua/saturn/plugins/misc.lua
@@ -294,7 +294,18 @@ return {
     "ziontee113/color-picker.nvim",
     cmd = { "PickColor", "PickColorInsert" },
     config = function()
-      require("color-picker")
+      require("color-picker").setup({
+        ["icons"] = { "ﱢ", "" },
+        ["border"] = "rounded", -- none | single | double | rounded | solid | shadow
+        ["keymap"] = { -- mapping example:
+          ["U"] = "<Plug>ColorPickerSlider5Decrease",
+          ["O"] = "<Plug>ColorPickerSlider5Increase",
+        },
+        ["background_highlight_group"] = "Normal", -- default
+        ["border_highlight_group"] = "FloatBorder", -- default
+        ["text_highlight_group"] = "Normal", --default
+      })
+      vim.cmd([[hi FloatBorder guibg=NONE]]) -- if you don't want weird border background colors around the popup.
     end,
   },
 
