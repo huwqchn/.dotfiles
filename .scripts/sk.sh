74c6868c4a896d0b8f3576df0cede54b624dbb8c
arch johnson <huwqchn@gmail.com>
2023-02-08 14:15:06 +0800

    add peek screenkey config

--- .scripts/sk.sh
+++ .scripts/sk.sh
@@ -1,6 +1,6 @@
 result=$(ps ax|grep -v grep|grep screenkey)
 if [ "$result" == "" ]; then
-  eval "screenkey --bg-color white --font-color black &"
+  eval "screenkey --opacity 0.0 --bg-color black --font-color white &"
 else
   eval "killall screenkey"
 fi
