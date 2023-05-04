1e0acb8a9e070ba0d84ee9b4ee77d5aae6265e73
arch johnson <huwqchn@gmail.com>
2023-05-04 12:40:57 +0800

    feat(nvim): jaq run code

--- nvim/.config/nvim/lua/saturn/plugins/coding/init.lua
+++ nvim/.config/nvim/lua/saturn/plugins/coding/init.lua
@@ -622,6 +622,78 @@ return {
     },
     config = true,
   },
+  {
+    "is0n/jaq-nvim",
+    keys = {
+      { "<leader>c<space>", "<cmd>silent only | Jaq<cr>", desc = "Run" },
+    },
+    opts = {
+      cmds = {
+        internal = {
+          lua = "luafile %",
+          vim = "source %",
+        },
+        external = {
+          typescript = "deno run %",
+          javascript = "node %",
+          -- markdown = "glow %",
+          python = "python %",
+          -- rust = "rustc % && ./$fileBase && rm $fileBase",
+          rust = "cargo run",
+          cpp = "g++ % -o $fileBase && ./$fileBase",
+          go = "go run %",
+          sh = "sh %",
+        },
+      },
+
+      behavior = {
+        -- Default type
+        default = "terminal",
+
+        -- Start in insert mode
+        startinsert = false,
+
+        -- Use `wincmd p` on startup
+        wincmd = false,
+
+        -- Auto-save files
+        autosave = false,
+      },
+
+      -- UI settings
+      ui = {
+        -- Floating Window / FTerm settings
+        float = {
+          -- Floating window border (see ':h nvim_open_win')
+          border = "rounded",
+
+          -- Num from `0 - 1` for measurements
+          height = 0.8,
+          width = 0.8,
+          x = 0.5,
+          y = 0.5,
+
+          -- Highlight group for floating window/border (see ':h winhl')
+          border_hl = "FloatBorder",
+          float_hl = "Normal",
+
+          -- Floating Window Transparency (see ':h winblend')
+          blend = 0,
+        },
+
+        terminal = {
+          -- Position of terminal
+          position = "vert",
+
+          -- Open the terminal without line numbers
+          line_no = false,
+
+          -- Size of terminal
+          size = 15,
+        },
+      },
+    },
+  },
   -- {
   --   "MattesGroeger/vim-bookmarks",
   --   config = true,
--- nvim/.config/nvim/lua/saturn/plugins/editor/init.lua
+++ nvim/.config/nvim/lua/saturn/plugins/editor/init.lua
@@ -1055,7 +1055,6 @@ return {
               threshold = vim.log.levels.INFO,
             },
             config = {
-              expand_lines = true,
               icons = { expanded = "", collapsed = "", circular = "" },
               mappings = {
                 -- Use a table to apply multiple mappings
@@ -1066,6 +1065,9 @@ return {
                 repl = "r",
                 toggle = "t",
               },
+              -- Use this to override mappings for specific elements
+              element_mappings = {},
+              expand_lines = true,
               layouts = {
                 {
                   elements = {
@@ -1086,6 +1088,21 @@ return {
                   position = "bottom",
                 },
               },
+              controls = {
+                enabled = true,
+                -- Display controls in this element
+                element = "repl",
+                icons = {
+                  pause = "",
+                  play = "",
+                  step_into = "",
+                  step_over = "",
+                  step_out = "",
+                  step_back = "",
+                  run_last = "",
+                  terminate = "",
+                },
+              },
               floating = {
                 max_height = 0.9,
                 max_width = 0.5, -- Floats will be treated as percentage of your screen.
@@ -1093,6 +1110,11 @@ return {
                 mappings = {
                   close = { "q", "<Esc>" },
                 },
+              },
+              windows = { indent = 1 },
+              render = {
+                max_type_length = nil, -- Can be integer or nil.
+                max_value_lines = 100, -- Can be integer or nil.
               },
             },
           },
