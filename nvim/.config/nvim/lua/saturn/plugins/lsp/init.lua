8b90e9ca42684380db00895548fdb084a4cc75f0
arch johnson <huwqchn@gmail.com>
2023-05-06 12:05:39 +0800

    chore(nvim): add mason keys

--- nvim/.config/nvim/lua/saturn/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/saturn/plugins/lsp/init.lua
@@ -121,7 +121,7 @@ return {
   },
   {
     "williamboman/mason.nvim",
-    cmd = "Mason",
+    cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     opts = {
       ui = {
