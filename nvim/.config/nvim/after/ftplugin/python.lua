64f19ae8d9167a59da188b285ef232e8e0426c30
Johnson Hu <huwqchn@gmail.com>
2023-05-31 19:05:14 +0800

    fix(nvim): lazy load dap

--- nvim/.config/nvim/after/ftplugin/python.lua
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -9,18 +9,18 @@ require("saturn.plugins.lsp.manager").setup("pyright")
 
 --Setup dap for python
 -- local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
-pcall(function()
-  -- require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
-  require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
-end)
+-- pcall(function()
+-- require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+-- require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+-- end)
 
 -- Supported test frameworks are unittest, pytest and django. By default it
 -- tries to detect the runner by probing for pytest.ini and manage.py, if
 -- neither are present it defaults to unittest.
-pcall(function()
-  require("dap-python").test_runner = "pytest"
-end)
+-- pcall(function()
+-- require("dap-python").test_runner = "pytest"
+-- end)
 
-vim.keymap.set("n", "<leader>dt", "<cmd>lua require('dap-python').test_method()<cr>", { desc = "Test Method" })
-vim.keymap.set("n", "<leader>dT", "<cmd>lua require('dap-python').test_class()<cr>", { desc = "Test Class" })
-vim.keymap.set("v", "<leader>ds", "<cmd>lua require('dap-python').debug_selection()<cr>", { desc = "Debug Selection" })
+-- vim.keymap.set("n", "<leader>dt", "<cmd>lua require('dap-python').test_method()<cr>", { desc = "Test Method" })
+-- vim.keymap.set("n", "<leader>dT", "<cmd>lua require('dap-python').test_class()<cr>", { desc = "Test Class" })
+-- vim.keymap.set("v", "<leader>ds", "<cmd>lua require('dap-python').debug_selection()<cr>", { desc = "Debug Selection" })
--- nvim/.config/nvim/lua/saturn/plugins/editor/init.lua
+++ nvim/.config/nvim/lua/saturn/plugins/editor/init.lua
@@ -1195,18 +1195,32 @@ return {
     "nvim-neotest/neotest",
     keys = {
       {
+        "<leader>td",
+        function()
+          require("neotest").run.run({ strategy = "dap" })
+        end,
+        desc = "Debug Nearest",
+      },
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run File",
+      },
+      {
+        "<leader>tT",
+        function()
+          require("neotest").run.run(vim.loop.cwd())
+        end,
+        desc = "Run All Test Files",
+      },
+      {
         "<leader>tr",
         function()
           require("neotest").run.run()
         end,
         desc = "Run Nearest",
-      },
-      {
-        "<leader>tR",
-        function()
-          require("neotest").run.run(vim.fn.expand("%"))
-        end,
-        desc = "Run File",
       },
       {
         "<leader>ts",
@@ -1230,25 +1244,11 @@ return {
         desc = "Toggle Output Panel",
       },
       {
-        "<leader>td",
-        function()
-          require("neotest").run.run({ strategy = "dap" })
-        end,
-        desc = "Debug Nearest",
-      },
-      {
         "<leader>tS",
         function()
           require("neotest").run.stop()
         end,
         desc = "Stop",
-      },
-      {
-        "<leader>tD",
-        function()
-          require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
-        end,
-        desc = "Debug File",
       },
     },
     opts = {
