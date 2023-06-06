b0946548157f79de1d6af494ad891e806b3e5919
Johnson Hu <huwqchn@gmail.com>
2023-06-06 12:25:37 +0800

    chore(kitty): remove extend keys

--- nvim/.config/nvim/lua/saturn/plugins/extra/comment.lua
+++ nvim/.config/nvim/lua/saturn/plugins/extra/comment.lua
@@ -29,8 +29,11 @@ return {
       },
     },
     opts = {
-      pre_hook = function()
-        require("ts_context_commentstring.internal").update_commentstring({})
+      pre_hook = function(...)
+        local loaded, ts_comment = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+        if loaded and ts_comment then
+          return ts_comment.create_pre_hook()(...)
+        end
       end,
     },
   },
