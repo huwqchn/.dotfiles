8353dea8f90bc7d0ff2cd6f813a0bd5f1e467cf5
Johnson Hu <huwqchn@gmail.com>
2023-06-13 17:50:43 +0800

    feat(nvim): add edgy and vscode support

--- nvim/.config/nvim/lua/plugins/extras/coding/symbols/symbols-outline.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/symbols/symbols-outline.lua
@@ -1,17 +1,18 @@
 return {
-	{
-		"simrat39/symbols-outline.nvim",
-		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-		opts = {
-			keymaps = {
-				close = { "<Esc>", "q" },
-				goto_location = "<Cr>",
-				focus_location = "<space>",
-				hover_symbol = "<C-h>",
-				toggle_preview = "I",
-				fold = "n",
-				unfold = "o",
-			},
-		},
-	},
+  {
+    "simrat39/symbols-outline.nvim",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    cmd = "SymbolsOutline",
+    opts = {
+      keymaps = {
+        close = { "<Esc>", "q" },
+        goto_location = "<Cr>",
+        focus_location = "<space>",
+        hover_symbol = "<C-h>",
+        toggle_preview = "I",
+        fold = "n",
+        unfold = "o",
+      },
+    },
+  },
 }
