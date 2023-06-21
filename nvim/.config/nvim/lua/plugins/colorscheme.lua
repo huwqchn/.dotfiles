45974bfab4e8da1a4bc4a992c24f36c3e4c4c668
Johnson <huwqchn@gmail.com>
2023-06-21 22:59:13 +0800

    fix(nvim,yabai): fix colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,24 @@
 local M = {}
 local colorscheme = os.getenv("THEME")
+if colorscheme == nil then
+  colorscheme = "tokyonight"
+end
 local theme_path = "plugins.extras.ui.theme." .. colorscheme
 local has_theme, _ = pcall(require, theme_path)
 if has_theme then
-	M = {
-		{ import = theme_path },
-		-- Configure LazyVim to load color scheme
-		{
-			"LazyVim/LazyVim",
-			opts = {
-				colorscheme = function()
-					vim.g.colors_name = colorscheme
-					vim.cmd.colorscheme(colorscheme)
-				end,
-			},
-		},
-	}
+  M = {
+    { import = theme_path },
+    -- Configure LazyVim to load color scheme
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = function()
+          vim.g.colors_name = colorscheme
+          vim.cmd.colorscheme(colorscheme)
+        end,
+      },
+    },
+  }
 end
 
 return M
