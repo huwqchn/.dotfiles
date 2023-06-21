01607f29df6085d47f90d39aeda1b6cccb48fac7
Johnson Hu <huwqchn@gmail.com>
2023-06-21 11:25:53 +0800

    feat(nvim): bind more window operation with 's'

--- nvim/.config/nvim/lua/plugins/extras/ui/windows/windows.lua
+++ nvim/.config/nvim/lua/plugins/extras/ui/windows/windows.lua
@@ -1,23 +1,25 @@
 return {
-	{
-		"anuvyklack/windows.nvim",
-		event = "WinNew",
-		keys = {
-			{ "<leader>wz", "<Cmd>WindowsMaximize<CR>", desc = "Zoom" },
-			{ "<leader>wv", "<Cmd>WindowsMaximizeVertically<CR>", desc = "Maximize Vertically" },
-			{ "<leader>wh", "<Cmd>WindowsMaximizeHorizontally<CR>", desc = "Maximize Horizontally" },
-			{ "<leader>wb", "<Cmd>WindowsEqualize<CR>", desc = "Balance" },
-		},
-		dependencies = {
-			{ "anuvyklack/middleclass" },
-			{ "anuvyklack/animation.nvim", enabled = false },
-		},
-		config = function()
-			vim.o.winwidth = 5
-			vim.o.equalalways = false
-			require("windows").setup({
-				animation = { enable = false, duration = 150 },
-			})
-		end,
-	},
+  {
+    "anuvyklack/windows.nvim",
+    event = "WinNew",
+    keys = {
+      { "<leader>wz", "<Cmd>WindowsMaximize<CR>", desc = "Zoom" },
+      { "sz", "<leader>wz", remap = true, desc = "Zoom window" },
+      { "<leader>wv", "<Cmd>WindowsMaximizeVertically<CR>", desc = "Maximize Vertically" },
+      { "<leader>wh", "<Cmd>WindowsMaximizeHorizontally<CR>", desc = "Maximize Horizontally" },
+      { "<leader>wb", "<Cmd>WindowsEqualize<CR>", desc = "Balance" },
+      { "sb", "<leader>wb", remap = true, desc = "Balance window" },
+    },
+    dependencies = {
+      { "anuvyklack/middleclass" },
+      { "anuvyklack/animation.nvim", enabled = false },
+    },
+    config = function()
+      vim.o.winwidth = 5
+      vim.o.equalalways = false
+      require("windows").setup({
+        animation = { enable = false, duration = 150 },
+      })
+    end,
+  },
 }
