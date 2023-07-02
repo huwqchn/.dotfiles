c6f746707e4e6144a6860912e82df6ed1a71e9a6
Johnson <huwqchn@gmail.com>
2023-07-02 16:11:39 +0800

    chore(nvim)

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -35,24 +35,24 @@ return {
     config = true,
   },
   -- pick color
-  {
-    "ziontee113/color-picker.nvim",
-    cmd = { "PickColor", "PickColorInsert" },
-    config = function()
-      require("color-picker").setup({
-        ["icons"] = { "ﱢ", "" },
-        ["border"] = "rounded", -- none | single | double | rounded | solid | shadow
-        ["keymap"] = { -- mapping example:
-          ["E"] = "<Plug>ColorPickerSlider5Decrease",
-          ["I"] = "<Plug>ColorPickerSlider5Increase",
-        },
-        ["background_highlight_group"] = "Normal", -- default
-        ["border_highlight_group"] = "FloatBorder", -- default
-        ["text_highlight_group"] = "Normal", --default
-      })
-      vim.cmd([[hi FloatBorder guibg=NONE]]) -- if you don't want weird border background colors around the popup.
-    end,
-  },
+  -- {
+  --   "ziontee113/color-picker.nvim",
+  --   cmd = { "PickColor", "PickColorInsert" },
+  --   config = function()
+  --     require("color-picker").setup({
+  --       ["icons"] = { "ﱢ", "" },
+  --       ["border"] = "rounded", -- none | single | double | rounded | solid | shadow
+  --       ["keymap"] = { -- mapping example:
+  --         ["E"] = "<Plug>ColorPickerSlider5Decrease",
+  --         ["I"] = "<Plug>ColorPickerSlider5Increase",
+  --       },
+  --       ["background_highlight_group"] = "Normal", -- default
+  --       ["border_highlight_group"] = "FloatBorder", -- default
+  --       ["text_highlight_group"] = "Normal", --default
+  --     })
+  --     vim.cmd([[hi FloatBorder guibg=NONE]]) -- if you don't want weird border background colors around the popup.
+  --   end,
+  -- },
   -- {
   --   "ecthelionvi/NeoComposer.nvim",
   --   dependencies = { "kkharji/sqlite.lua" },
