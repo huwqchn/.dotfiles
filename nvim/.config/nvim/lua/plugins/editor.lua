9d9b7e4e7b7da3e1d09e0a03d6a4dc2c7599e9ec
Johnson <huwqchn@gmail.com>
2023-07-03 23:44:17 +0800

    chore(nvim): use lazyvim project instead of my project config

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -2,7 +2,6 @@ return {
   { import = "plugins.extras.editor.explorer.neo-tree" },
   { import = "plugins.extras.editor.search.telescope" },
   { import = "plugins.extras.editor.search.ssr" },
-  { import = "plugins.extras.editor.search.project" },
   { import = "plugins.extras.editor.fold.nvim-ufo" },
   { import = "plugins.extras.editor.git.neogit" },
   { import = "plugins.extras.editor.git.misc" },
