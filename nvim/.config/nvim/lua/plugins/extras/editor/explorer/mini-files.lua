bf197d2e8e3cf89f4e6772005ded22ee6dc08b9e
Johnson Hu <huwqchn@gmail.com>
2023-07-05 14:02:35 +0800

    feat(nvim): use mini.files

--- nvim/.config/nvim/lua/plugins/extras/editor/explorer/mini-files.lua
+++ nvim/.config/nvim/lua/plugins/extras/editor/explorer/mini-files.lua
@@ -10,5 +10,9 @@ return {
         go_out_plus = "N",
       },
     },
+    keys = {
+      { "sm", "<leader>fm", desc = "Mini-files (directory of current file)", remap = true },
+      { "sM", "<leader>fM", desc = "Mini-files (cwd)", remap = true },
+    },
   },
 }
