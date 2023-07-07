58d72722d68e88ed9654945a5a6c3bbaa56d2bf8
Johnson Hu <huwqchn@gmail.com>
2023-07-07 11:36:37 +0800

    feat(nvim): rust add some keys

--- nvim/.config/nvim/lua/plugins/extras/lang/rust.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/rust.lua
@@ -10,6 +10,46 @@ return {
           break
         end
       end
+      local keys = {
+        {
+          "I",
+          "<cmd>RustHoverRange<cr>",
+          desc = "Hover Range (Rust)",
+          mode = "x",
+        },
+        {
+          "<leader>ce",
+          "<cmd>RustExpandMacro<cr>",
+          desc = "Expand Macro (Rust)",
+        },
+        {
+          "<leader>c,",
+          "<cmd>RustRunnables<cr>",
+          desc = "Run (Rust)",
+          mode = { "n", "x" },
+        },
+        {
+          "<leader>co",
+          "<cmd>RustOpenCargo<cr>",
+          desc = "Open Cargo (Rust)",
+        },
+        {
+          "s<space>",
+          "<cmd>RustParentModule<cr>",
+          desc = "Parent Module (Rust)",
+        },
+        {
+          "<leadea>c<Up>",
+          "<cmd>RustMoveItemUp<cr>",
+          desc = "Move Item up (Rust)",
+        },
+        {
+          "s<Down>",
+          "<cmd>RustMoveItemDown<cr>",
+          desc = "Move Item down (Rust)",
+        },
+      }
+      vim.list_extend(opts.servers.rust_analyzer.keys, keys)
       for _, key in ipairs(opts.servers.taplo.keys) do
         if key[1] == "K" then
           key[1] = "I"
