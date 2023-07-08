3bed0e9c4fa60ae806d421dc50e247605d8007d1
Johnson <huwqchn@gmail.com>
2023-07-08 16:09:37 +0800

    chore(nvim): tab -> accept tabnine advice

--- nvim/.config/nvim/lua/plugins/extras/coding/ai/tabnine.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/ai/tabnine.lua
@@ -44,7 +44,7 @@ return {
     config = function()
       require("tabnine").setup({
         disable_auto_comment = true,
-        accept_keymap = "<M-Tab>",
+        accept_keymap = "<Tab>",
         dismiss_keymap = "<C-c>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
