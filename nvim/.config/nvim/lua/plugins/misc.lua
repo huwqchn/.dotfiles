b2824d0e760db5d6283765f12b0365bb57cd1094
Johnson Hu <huwqchn@gmail.com>
2023-07-10 10:57:41 +0800

    feat(nvim): using neoswap to use emacs transpose like

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -73,4 +73,20 @@ return {
   -- { import = "plugins.extras.misc.smart-splits" },
   { import = "plugins.extras.misc.zen-mode" },
   -- { import = "plugins.extras.misc.harpoon" },
+  {
+    "ecthelionvi/NeoSwap.nvim",
+    keys = {
+      {
+        "st",
+        "<cmd>NeoSwapNext<cr>",
+        desc = "swap word to next",
+      },
+      {
+        "sT",
+        "<cmd>NeoSwapPrev<cr>",
+        desc = "swap word to previous",
+      },
+    },
+    opts = {},
+  },
 }
