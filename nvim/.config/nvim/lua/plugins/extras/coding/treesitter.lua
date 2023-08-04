b0350d5000a0c61acefb6275f85dec02039319cb
Johnson <huwqchn@gmail.com>
2023-08-04 23:24:19 +0800

    fix(nvim): fix treesitter playground keymaps

--- nvim/.config/nvim/lua/plugins/extras/coding/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/treesitter.lua
@@ -14,13 +14,6 @@ return {
   {
     "nvim-treesitter/playground",
     cmd = "TSPlaygroundToggle",
-    opts = {
-      keybindings = {
-        toggle_query_editor = "l",
-        toggle_hl_groups = "h",
-        toggle_language_display = "H",
-      },
-    },
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
@@ -101,11 +94,11 @@ return {
         updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
         persist_queries = true, -- Whether the query persists across vim sessions
         keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
+          toggle_query_editor = "l",
+          toggle_hl_groups = "h",
           toggle_injected_languages = "t",
           toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
+          toggle_language_display = "H",
           focus_language = "f",
           unfocus_language = "F",
           update = "R",
