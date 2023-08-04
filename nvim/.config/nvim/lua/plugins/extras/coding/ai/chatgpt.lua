fb5135b34f862911d925e9371a7010f55deb2c83
Johnson Hu <huwqchn@gmail.com>
2023-08-04 19:19:19 +0800

    feat(nvim): add chatgpt plugin and latex snips

--- nvim/.config/nvim/lua/plugins/extras/coding/ai/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/ai/chatgpt.lua
@@ -31,4 +31,33 @@ return {
       "nvim-telescope/telescope.nvim",
     },
   },
+  {
+    "james1236/backseat.nvim",
+    cmd = {
+      "Backseat",
+      "BackseatAsk",
+      "BackseatClear",
+      "BackseatClearLine",
+    },
+    keys = {
+      {
+        "<leader>ab",
+        "<cmd>Backseat<cr>",
+        desc = "backseat",
+      },
+    },
+    config = function()
+      require("backseat").setup({
+        -- Alternatively, set the env var $OPENAI_API_KEY by putting "export OPENAI_API_KEY=sk-xxxxx" in your ~/.bashrc
+        openai_model_id = "gpt-3.5-turbo", --gpt-4 (If you do not have access to a model, it says "The model does not exist")
+        -- language = 'english', -- Such as 'japanese', 'french', 'pirate', 'LOLCAT'
+        -- split_threshold = 100,
+        -- additional_instruction = "Respond snarkily", -- (GPT-3 will probably deny this request, but GPT-4 complies)
+        -- highlight = {
+        --     icon = '', -- ''
+        --     group = 'Comment',
+        -- }
+      })
+    end,
+  },
 }
--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -10,4 +10,5 @@ return {
   { import = "plugins.extras.lang.markdown" },
   { import = "plugins.extras.lang.http" },
   { import = "plugins.extras.lang.ymal" },
+  { import = "plugins.extras.lang.latex" },
 }
