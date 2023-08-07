e17a08eacb299d2eaf0e0fec169b8ea2bac379a3
Johnson Hu <huwqchn@gmail.com>
2023-08-07 13:25:18 +0800

    fix(nvim): commend lua-language-server opts

--- nvim/.config/nvim/lua/plugins/extras/lang/lua.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/lua.lua
@@ -11,76 +11,76 @@ return {
       },
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        lua_ls = {
-          -- enabled = false,
-          -- cmd = { "/home/folke/projects/lua-language-server/bin/lua-language-server" },
-          single_file_support = true,
-          settings = {
-            Lua = {
-              telemetry = { enable = false },
-              workspace = {
-                checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true),
-              },
-              completion = {
-                workspaceWord = true,
-                callSnippet = "Both",
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              misc = {
-                parameters = {
-                  -- "--log-level=trace",
-                },
-              },
-              diagnostics = {
-                disable = { "incomplete-signature-doc" },
-                -- enable = false,
-                groupSeverity = {
-                  strong = "Warning",
-                  strict = "Warning",
-                },
-                groupFileStatus = {
-                  ["ambiguity"] = "Opened",
-                  ["await"] = "Opened",
-                  ["codestyle"] = "None",
-                  ["duplicate"] = "Opened",
-                  ["global"] = "Opened",
-                  ["luadoc"] = "Opened",
-                  ["redefined"] = "Opened",
-                  ["strict"] = "Opened",
-                  ["strong"] = "Opened",
-                  ["type-check"] = "Opened",
-                  ["unbalanced"] = "Opened",
-                  ["unused"] = "Opened",
-                },
-                unusedLocalExclude = { "_*" },
-              },
-              format = {
-                enable = false,
-                defaultConfig = {
-                  indent_style = "space",
-                  indent_size = "2",
-                  continuation_indent_size = "2",
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       lua_ls = {
+  --         -- enabled = false,
+  --         -- cmd = { "/home/folke/projects/lua-language-server/bin/lua-language-server" },
+  --         single_file_support = true,
+  --         settings = {
+  --           Lua = {
+  --             telemetry = { enable = false },
+  --             workspace = {
+  --               checkThirdParty = false,
+  --               library = vim.api.nvim_get_runtime_file("", true),
+  --             },
+  --             completion = {
+  --               workspaceWord = true,
+  --               callSnippet = "Both",
+  --             },
+  --             hint = {
+  --               enable = true,
+  --               setType = false,
+  --               paramType = true,
+  --               paramName = "Disable",
+  --               semicolon = "Disable",
+  --               arrayIndex = "Disable",
+  --             },
+  --             doc = {
+  --               privateName = { "^_" },
+  --             },
+  --             misc = {
+  --               parameters = {
+  --                 -- "--log-level=trace",
+  --               },
+  --             },
+  --             diagnostics = {
+  --               disable = { "incomplete-signature-doc" },
+  --               -- enable = false,
+  --               groupSeverity = {
+  --                 strong = "Warning",
+  --                 strict = "Warning",
+  --               },
+  --               groupFileStatus = {
+  --                 ["ambiguity"] = "Opened",
+  --                 ["await"] = "Opened",
+  --                 ["codestyle"] = "None",
+  --                 ["duplicate"] = "Opened",
+  --                 ["global"] = "Opened",
+  --                 ["luadoc"] = "Opened",
+  --                 ["redefined"] = "Opened",
+  --                 ["strict"] = "Opened",
+  --                 ["strong"] = "Opened",
+  --                 ["type-check"] = "Opened",
+  --                 ["unbalanced"] = "Opened",
+  --                 ["unused"] = "Opened",
+  --               },
+  --               unusedLocalExclude = { "_*" },
+  --             },
+  --             format = {
+  --               enable = false,
+  --               defaultConfig = {
+  --                 indent_style = "space",
+  --                 indent_size = "2",
+  --                 continuation_indent_size = "2",
+  --               },
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
