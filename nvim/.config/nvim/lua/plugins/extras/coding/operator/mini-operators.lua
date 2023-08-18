169813c73e0f7b2516af4f00857f4049498335d6
Johnson <huwqchn@gmail.com>
2023-08-18 21:30:33 +0800

    feat(nvim): shortcut for swap words use mini.operators

--- nvim/.config/nvim/lua/plugins/extras/coding/operator/mini-operators.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/operator/mini-operators.lua
@@ -2,6 +2,10 @@ return {
   {
     "echasnovski/mini.operators",
     event = "BufRead",
+    keys = {
+      { "<M-t>", "sthww.", desc = "transform word after", remap = true },
+      { "<M-S-t>", "sthwW.", desc = "transform word before", remap = true },
+    },
     opts = {
       exchange = {
         prefix = "st",
