6a4239b0506d9a204b020c15869df09a67298c8b
Johnson Hu <huwqchn@gmail.com>
2023-08-21 11:06:13 +0800

    chore(nvim)

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- import LazyVim plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", version = "^2.0" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {} },
     -- lazyvim editor extension modules
     { import = "lazyvim.plugins.extras.util.project" },
     -- lazyvim dap core extension modules
