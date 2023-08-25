2bc74a3625d9b8309cd6870f25963d41e276a55d
Johnson Hu <huwqchn@gmail.com>
2023-08-25 18:24:38 +0800

    fix(nvim): fix cscope config

--- nvim/.config/nvim/lua/plugins/extras/editor/search/cscope.lua
+++ nvim/.config/nvim/lua/plugins/extras/editor/search/cscope.lua
@@ -84,26 +84,24 @@ return {
       },
     },
     opts = {
-      {
-        -- maps related defaults
-        disable_maps = true, -- "true" disables default keymaps
-        skip_input_prompt = false, -- "true" doesn't ask for input
+      -- maps related defaults
+      disable_maps = true, -- "true" disables default keymaps
+      skip_input_prompt = false, -- "true" doesn't ask for input
 
-        -- cscope related defaults
-        cscope = {
-          -- location of cscope db file
-          db_file = "./cscope.out",
-          -- cscope executable
-          exec = "cscope", -- "cscope" or "gtags-cscope"
-          -- choose your fav picker
-          picker = "telescope", -- "telescope", "fzf-lua" or "quickfix"
-          -- "true" does not open picker for single result, just JUMP
-          skip_picker_for_single_result = false, -- "false" or "true"
-          -- these args are directly passed to "cscope -f <db_file> <args>"
-          db_build_cmd_args = { "-bqkv" },
-          -- statusline indicator, default is cscope executable
-          statusline_indicator = nil,
-        },
+      -- cscope related defaults
+      cscope = {
+        -- location of cscope db file
+        db_file = "~/Repos/linux/cscope.out",
+        -- cscope executable
+        exec = "cscope", -- "cscope" or "gtags-cscope"
+        -- choose your fav picker
+        picker = "telescope", -- "telescope", "fzf-lua" or "quickfix"
+        -- "true" does not open picker for single result, just JUMP
+        skip_picker_for_single_result = false, -- "false" or "true"
+        -- these args are directly passed to "cscope -f <db_file> <args>"
+        db_build_cmd_args = { "-bqkv" },
+        -- statusline indicator, default is cscope executable
+        statusline_indicator = nil,
       },
     },
   },
