5b9131f7915bcfaf3bf8a794ed72b5ca862d1165
Johnson Hu <huwqchn@gmail.com>
2023-09-21 11:41:40 +0800

    feat(zsh,zellij): autostart zellij

--- zsh/.config/zsh/eval.zsh
+++ zsh/.config/zsh/eval.zsh
@@ -2,4 +2,5 @@ eval "$(zoxide init zsh)"
 eval "$(starship init zsh)"
 eval "$(atuin init zsh)"
 eval "$(navi widget zsh)"
+eval "$(zellij setup --generate-auto-start zsh)"
 # source /opt/anaconda/bin/activate root
