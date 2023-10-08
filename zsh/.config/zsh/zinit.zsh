fdf0e2b2b01ce607738bb10f965ef251744bd382
Johnson Hu <huwqchn@gmail.com>
2023-10-08 09:55:38 +0800

    chore(zsh): use zinit auto install cmdline

--- zsh/.config/zsh/zinit.zsh
+++ zsh/.config/zsh/zinit.zsh
@@ -17,6 +17,8 @@ zinit light-mode for \
 # zinit ice wait lucid
 # zinit load zdharma-continuum/history-search-multi-word
 
+# SSH-AGENT
+zinit light bobsoppe/zsh-ssh-agent
 zinit light zdharma-continuum/fast-syntax-highlighting
 # AUTOSUGGESTIONS, TRIGGER PRECMD HOOK UPON LOAD
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
@@ -25,6 +27,9 @@ HISTSIZE=290000
 SAVEHIST=$HISTSIZE
 zinit ice wait="0a" lucid atload="_zsh_autosuggest_start"
 zinit light zsh-users/zsh-autosuggestions
+# ZOXIDE
+zinit ice wait="0" lucid from="gh-r" as="program" pick="zoxide-*/zoxide -> zoxide" cp="zoxide-*/completions/_zoxide -> _zoxide" atclone="./zoxide init zsh > init.zsh" atpull="%atclone" src="init.zsh"
+zinit light ajeetdsouza/zoxide
 # HISTORY
 zinit ice wait="0b" lucid atload'bindkey "$terminfo[kcuu1]" history-substring-search-up; bindkey "$terminfo[kcud1]" history-substring-search-down'
 zinit light zsh-users/zsh-history-substring-search
@@ -48,7 +53,16 @@ zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm,cmd
 zstyle ':fzf-tab:complete:kill:argument-rest' extra-opts --preview=$extract'ps --pid=$in[(w)1] -o cmd --no-headers -w -w' --preview-window=down:3:wrap
 zstyle ":completion:*:git-checkout:*" sort false
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-
+# FZF
+zinit ice from="gh-r" as="command" bpick="*linux_amd64*"
+zinit light junegunn/fzf
+# FZF BYNARY AND TMUX HELPER SCRIPT
+zinit ice lucid wait'0c' as="command" id-as="junegunn/fzf-tmux" pick="bin/fzf-tmux"
+zinit light junegunn/fzf
+# BIND MULTIPLE WIDGETS USING FZF
+zinit ice lucid wait'0c' multisrc"shell/{completion,key-bindings}.zsh" id-as="junegunn/fzf_completions" pick="/dev/null"
+zinit light junegunn/fzf
+# FZF-TAB
 zinit ice wait="1" lucid
 zinit light Aloxaf/fzf-tab
 zinit wait="0" lucid light-mode for \
@@ -57,4 +71,30 @@ zinit wait="0" lucid light-mode for \
     wfxr/forgit
 
 # TODO: add zinit auto install cmdline tools, git, nvim, tmux, ezd, fzf, etc
+
+# RIPGREP
+zinit ice from="gh-r" as="program" bpick="*amd64.deb" pick="usr/bin/rg"
+zinit light BurntSushi/ripgrep
+# NEOVIM
+zinit ice from="gh-r" as="program" bpick="*linux64.tar.gz" ver="nightly" pick="nvim-linux64/bin/nvim" atload="alias v=nvim"
+zinit light neovim/neovim
+# LAZYGIT
+zinit ice lucid wait="0" as="program" from="gh-r" bpick="*Linux_x86_64*" pick="lazygit" atload="alias lg='lazygit'"
+zinit light jesseduffield/lazygit
+# LAZYDOCKER
+zinit ice lucid wait="0" as="program" from="gh-r" bpick="*Linux_x86_64*" pick="lazydocker" atload="alias lzd='lazydocker'"
+zinit light jesseduffield/lazydocker
+# FD
+zinit ice as="command" from="gh-r" bpick="*amd64.deb" pick="usr/bin/fd"
+zinit light sharkdp/fd
+# DELTA
+zinit ice lucid wait="0" as="program" from="gh-r" bpick="*amd64.deb" pick="usr/bin/delta"
+zinit light dandavison/delta
+# DUST
+zinit ice wait="2" lucid from="gh-r" as="program" bpick="*amd64.deb" pick="usr/bin/dust" atload="alias du=dust"
+zinit light bootandy/dust
+# SAD
+zinit ice from="gh-r" as="command"
+zinit light ms-jpq/sad
+
 bindkey ',' autosuggest-accept
