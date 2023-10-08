4fdbcdd2a143fb5ee1db5f8ca357ecde436bf907
Johnson Hu <huwqchn@gmail.com>
2023-10-08 18:19:57 +0800

    feat(nvim): update lazyvim config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -27,6 +27,9 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.formatting.conform" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- lang
     { import = "lazyvim.plugins.extras.lang.clangd" },
