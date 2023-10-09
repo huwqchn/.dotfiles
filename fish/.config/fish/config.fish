ded973dd13ef85482be60aead43d658fd7ba8244
Johnson Hu <huwqchn@gmail.com>
2023-10-09 16:35:20 +0800

    feat(fish): some abbr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,3 +13,19 @@ fish_add_path /snap/bin
 
 # FISH
 set fish_emoji_width 2
+
+# arch package manager
+abbr P 'sudo pacman -'
+abbr Pi "sudo pacman -S"
+abbr Pr "sudo pacman -Rns"
+abbr Ps "sudo pacman -Ss"
+abbr Pl "sudo pacman -Q"
+abbr Pf "sudo pacman -Ql"
+abbr Po "sudo pacman -Qo"
+abbr p 'paru -'
+abbr pi "paru -S"
+abbr pr "paru -Rns"
+abbr ps "paru -Ss"
+abbr pl "paru -Q"
+abbr pf "paru -Ql"
+abbr po "paru -Qo"
