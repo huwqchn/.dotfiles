af26620b99d999df1f8aaff55ae85dcb4715fc23
Johnson Hu <huwqchn@gmail.com>
2023-10-11 10:30:32 +0800

    chore(nvim)

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -2,7 +2,7 @@ return {
   -- { import = "plugins.extras.ui.animate.mini-animate" },
   { import = "plugins.extras.ui.dress.noice" },
   { import = "plugins.extras.ui.dress.notify" },
-  { import = "plugins.extras.ui.dashboard.alpha" },
+  -- { import = "plugins.extras.ui.dashboard.alpha" },
   { import = "plugins.extras.ui.icons" },
   { import = "plugins.extras.ui.bar.bufferline" },
   { import = "plugins.extras.ui.bar.lualine" },
