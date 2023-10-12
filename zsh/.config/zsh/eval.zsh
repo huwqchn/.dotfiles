fed0203198c425f9e494cebcf1805eef761ca3cf
Johnson Hu <huwqchn@gmail.com>
2023-10-12 13:56:10 +0800

    feat(zsh): not to auto activate tmux and conda, so many bugs

--- zsh/.config/zsh/eval.zsh
+++ zsh/.config/zsh/eval.zsh
@@ -3,24 +3,24 @@ eval "$(starship init zsh --print-full-init)"
 eval "$(atuin init zsh --disable-up-arrow)"
 eval "$(navi widget zsh)"
 # eval "$(zellij setup --generate-auto-start zsh)"
-if  [ -z "$TMUX" ]
-then
-  tmux attach -t main || tmux new -s main
-fi
+# if  [ -z "$TMUX" ]
+# then
+#   tmux attach -t main || tmux new -s main
+# fi
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/anaconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/anaconda/etc/profile.d/conda.sh" ]; then
-        . "/opt/anaconda/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/anaconda/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+# __conda_setup="$('/opt/anaconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/opt/anaconda/etc/profile.d/conda.sh" ]; then
+#         . "/opt/anaconda/etc/profile.d/conda.sh"
+#     else
+#         export PATH="/opt/anaconda/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
 # <<< conda initialize <<<
 
 # neofetch initialize
