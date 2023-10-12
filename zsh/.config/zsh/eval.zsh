8183c3a35cfc06af0f653cc9a1b933d2f3d2d4c2
Johnson Hu <huwqchn@gmail.com>
2023-10-12 11:05:30 +0800

    feat(zsh): init conda in zsh

--- zsh/.config/zsh/eval.zsh
+++ zsh/.config/zsh/eval.zsh
@@ -7,4 +7,31 @@ if  [ -z "$TMUX" ]
 then
   tmux attach -t main || tmux new -s main
 fi
-# source /opt/anaconda/bin/activate root
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/anaconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/anaconda/etc/profile.d/conda.sh" ]; then
+        . "/opt/anaconda/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/anaconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# neofetch initialize
+# # Check if neofetch is installed
+# if command -v neofetch &> /dev/null; then
+#     # Check if the configuration file exists
+#     if [ -f $HOME/.config/neofetch/config_tokyonight.conf ]; then
+#         neofetch --config $HOME/.config/neofetch/config_tokyonight.conf
+#     else
+#         neofetch
+#     fi
+# else
+#     echo "neofetch is not installed."
+# fi
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -8,15 +8,3 @@ source ~/.config/zsh/eval.zsh
 
 # beeping is annoying
 unsetopt BEEP
-
-# # Check if neofetch is installed
-# if command -v neofetch &> /dev/null; then
-#     # Check if the configuration file exists
-#     if [ -f $HOME/.config/neofetch/config_tokyonight.conf ]; then
-#         neofetch --config $HOME/.config/neofetch/config_tokyonight.conf
-#     else
-#         neofetch
-#     fi
-# else
-#     echo "neofetch is not installed."
-# fi
