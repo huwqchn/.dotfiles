2cad2501ce3ea91b63bc743a5a5f7d99d5b80ab9
Johnson Hu <huwqchn@gmail.com>
2023-10-13 18:07:16 +0800

    refactor(nvim): use lazyextras handle all my extras plugins

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -13,11 +13,4 @@ return {
       filetype_exclude = { "markdown", "diff" },
     },
   },
-  { import = "plugins.extras.misc.dial" },
-  { import = "plugins.extras.misc.smart-splits" },
-  { import = "plugins.extras.misc.zen-mode" },
-  -- { import = "plugins.extras.misc.image" },
-  -- { import = "plugins.extras.misc.harpoon" },
-  -- { import = "plugins.extras.misc.ghost-text" },
-  -- { import = "plugins.extras.misc.neoswap" },
 }
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,14 +1,4 @@
 return {
-  -- { import = "plugins.extras.ui.animate.mini-animate" },
-  { import = "plugins.extras.ui.dress.noice" },
-  { import = "plugins.extras.ui.dress.notify" },
-  -- { import = "plugins.extras.ui.dashboard.alpha" },
-  { import = "plugins.extras.ui.icons" },
-  { import = "plugins.extras.ui.bar.bufferline" },
+  --TODO: need to refactor this
   { import = "plugins.extras.ui.bar.lualine" },
-  -- { import = "plugins.extras.ui.bar.breadcrumbs" },
-  { import = "plugins.extras.ui.bar.scrollbar" },
-  { import = "plugins.extras.ui.bar.dropbar" },
-  { import = "plugins.extras.ui.windows.windows" },
-  { import = "plugins.extras.ui.windows.window-picker" },
 }
