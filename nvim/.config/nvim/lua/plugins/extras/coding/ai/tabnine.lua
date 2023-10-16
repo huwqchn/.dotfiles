07801db7f218d18c529b61121e56c0c434f16710
Johnson Hu <huwqchn@gmail.com>
2023-10-16 10:14:59 +0800

    feat(tabnine): change keybindings

--- nvim/.config/nvim/lua/plugins/extras/coding/ai/tabnine.lua
+++ nvim/.config/nvim/lua/plugins/extras/coding/ai/tabnine.lua
@@ -44,8 +44,8 @@ return {
     config = function()
       require("tabnine").setup({
         disable_auto_comment = true,
-        accept_keymap = "<M-Tab>",
-        dismiss_keymap = "<C-c>",
+        accept_keymap = "<M-a>",
+        dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
         exclude_filetypes = { "TelescopePrompt" },
