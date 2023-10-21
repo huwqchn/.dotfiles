bf2f3d0196539460ad1161e02aeed28d8cbbcb68
Johnson <huwqchn@gmail.com>
2023-10-22 01:49:38 +0800

    fix(wezterm): dont change window size when changing font size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -82,4 +82,6 @@ config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 -- cell_width = 0.9,
 config.scrollback_lines = 10000
 
+config.adjust_window_size_when_changing_font_size = false
+
 return config --[[@as Wezterm]]
