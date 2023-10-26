efc7d5da4e58c603f0e89daf1029f056f16ccc0f
johnson hu <huwqchn@gmail.com>
2023-10-26 11:21:01 +0800

    feat(script): add local scripts

---
+++ home/dot_scripts/executable_chatgpt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+INPUT=$(rofi -dmenu -p " 󰚩  " -icon-theme "Papirus" -show-icons -config ~/.config/rofi/search.rasi)
+if [[ -z $INPUT ]]; then
+    exit 1
+fi
+
+zenity --progress --text="Waiting for an answer" --pulsate &
+
+if [[ $? -eq 1 ]]; then
+    exit 1
+fi
+
+PID=$!
+
+ANSWER=$(~/.local/bin/sgpt "$INPUT" --no-animation --no-spinner)
+kill $PID
+zenity --info --text="$ANSWER"
