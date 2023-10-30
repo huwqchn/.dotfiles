ad1d9ec9058391f0abf4f6c34d763c56a0a1d78d
johnson hu <huwqchn@gmail.com>
2023-10-30 16:24:39 +0800

    feat(wezterm):change wezterm copy mode

--- home/private_dot_config/wezterm/keys.lua
+++ home/private_dot_config/wezterm/keys.lua
@@ -102,6 +102,248 @@ function M.setup(config)
 		M.smart_nav("move", "CTRL", "i", "Up"),
 		M.smart_nav("move", "CTRL", "o", "Right"),
 	}
+	config.key_tables = {
+		copy_mode = {
+			{ key = "Tab", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+			{
+				key = "Tab",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveBackwardWord"),
+			},
+			{
+				key = "Enter",
+				mods = "NONE",
+				action = act.CopyMode("MoveToStartOfNextLine"),
+			},
+			{ key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
+			{
+				key = "Space",
+				mods = "NONE",
+				action = act.CopyMode({ SetSelectionMode = "Cell" }),
+			},
+			{
+				key = "$",
+				mods = "NONE",
+				action = act.CopyMode("MoveToEndOfLineContent"),
+			},
+			{
+				key = "$",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToEndOfLineContent"),
+			},
+			{ key = "n", mods = "NONE", action = act.CopyMode("MoveLeft") },
+			{ key = "e", mods = "NONE", action = act.CopyMode("MoveDown") },
+			{ key = "i", mods = "NONE", action = act.CopyMode("MoveUp") },
+			{ key = "o", mods = "NONE", action = act.CopyMode("MoveRight") },
+			{
+				key = "O",
+				mods = "NONE",
+				action = act.CopyMode("MoveToStartOfLineContent"),
+			},
+			{
+				key = "O",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToEndOfLineContent"),
+			},
+			{
+				key = "N",
+				mods = "NONE",
+				action = act.CopyMode("MoveToEndOfLineContent"),
+			},
+			{
+				key = "N",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToStartOfLineContent"),
+			},
+			{ key = "I", mods = "NONE", action = act.CopyMode("MoveToViewportTop") },
+			{
+				key = "I",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToViewportTop"),
+			},
+			{
+				key = "E",
+				mods = "NONE",
+				action = act.CopyMode("MoveToViewportBottom"),
+			},
+			{
+				key = "E",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToViewportBottom"),
+			},
+			{
+				key = "j",
+				mods = "NONE",
+				action = act.CopyMode("MoveForwardWordEnd"),
+			},
+			{
+				key = "L",
+				mods = "NONE",
+				action = act.CopyMode("MoveToSelectionOtherEndHoriz"),
+			},
+			{
+				key = "L",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToSelectionOtherEndHoriz"),
+			},
+			{
+				key = "l",
+				mods = "NONE",
+				action = act.CopyMode("MoveToSelectionOtherEnd"),
+			},
+			{ key = ":", mods = "NONE", action = act.CopyMode("JumpReverse") },
+			{ key = ":", mods = "SHIFT", action = act.CopyMode("JumpReverse") },
+			{ key = "0", mods = "NONE", action = act.CopyMode("MoveToStartOfLine") },
+			{ key = ";", mods = "NONE", action = act.CopyMode("JumpAgain") },
+			{
+				key = "F",
+				mods = "NONE",
+				action = act.CopyMode({ JumpBackward = { prev_char = false } }),
+			},
+			{
+				key = "F",
+				mods = "SHIFT",
+				action = act.CopyMode({ JumpBackward = { prev_char = false } }),
+			},
+			{
+				key = "G",
+				mods = "NONE",
+				action = act.CopyMode("MoveToScrollbackBottom"),
+			},
+			{
+				key = "G",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToScrollbackBottom"),
+			},
+			{
+				key = "M",
+				mods = "NONE",
+				action = act.CopyMode("MoveToViewportMiddle"),
+			},
+			{
+				key = "M",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToViewportMiddle"),
+			},
+			{
+				key = "T",
+				mods = "NONE",
+				action = act.CopyMode({ JumpBackward = { prev_char = true } }),
+			},
+			{
+				key = "T",
+				mods = "SHIFT",
+				action = act.CopyMode({ JumpBackward = { prev_char = true } }),
+			},
+			{
+				key = "V",
+				mods = "NONE",
+				action = act.CopyMode({ SetSelectionMode = "Line" }),
+			},
+			{
+				key = "V",
+				mods = "SHIFT",
+				action = act.CopyMode({ SetSelectionMode = "Line" }),
+			},
+			{
+				key = "^",
+				mods = "NONE",
+				action = act.CopyMode("MoveToStartOfLineContent"),
+			},
+			{
+				key = "^",
+				mods = "SHIFT",
+				action = act.CopyMode("MoveToStartOfLineContent"),
+			},
+			{ key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
+			{ key = "b", mods = "ALT", action = act.CopyMode("MoveBackwardWord") },
+			{ key = "b", mods = "CTRL", action = act.CopyMode("PageUp") },
+			{ key = "c", mods = "CTRL", action = act.CopyMode("Close") },
+			{
+				key = "d",
+				mods = "CTRL",
+				action = act.CopyMode({ MoveByPage = 0.5 }),
+			},
+			{
+				key = "f",
+				mods = "NONE",
+				action = act.CopyMode({ JumpForward = { prev_char = false } }),
+			},
+			{ key = "f", mods = "ALT", action = act.CopyMode("MoveForwardWord") },
+			{ key = "f", mods = "CTRL", action = act.CopyMode("PageDown") },
+			{
+				key = "g",
+				mods = "NONE",
+				action = act.CopyMode("MoveToScrollbackTop"),
+			},
+			{ key = "g", mods = "CTRL", action = act.CopyMode("Close") },
+			{
+				key = "m",
+				mods = "ALT",
+				action = act.CopyMode("MoveToStartOfLineContent"),
+			},
+			{ key = "q", mods = "NONE", action = act.CopyMode("Close") },
+			{
+				key = "t",
+				mods = "NONE",
+				action = act.CopyMode({ JumpForward = { prev_char = true } }),
+			},
+			{
+				key = "u",
+				mods = "CTRL",
+				action = act.CopyMode({ MoveByPage = -0.5 }),
+			},
+			{
+				key = "v",
+				mods = "NONE",
+				action = act.CopyMode({ SetSelectionMode = "Cell" }),
+			},
+			{
+				key = "v",
+				mods = "CTRL",
+				action = act.CopyMode({ SetSelectionMode = "Block" }),
+			},
+			{ key = "w", mods = "NONE", action = act.CopyMode("MoveForwardWord") },
+			{
+				key = "y",
+				mods = "NONE",
+				action = act.Multiple({
+					{ CopyTo = "ClipboardAndPrimarySelection" },
+					{ CopyMode = "Close" },
+				}),
+			},
+			{ key = "PageUp", mods = "NONE", action = act.CopyMode("PageUp") },
+			{ key = "PageDown", mods = "NONE", action = act.CopyMode("PageDown") },
+			{
+				key = "End",
+				mods = "NONE",
+				action = act.CopyMode("MoveToEndOfLineContent"),
+			},
+			{
+				key = "Home",
+				mods = "NONE",
+				action = act.CopyMode("MoveToStartOfLine"),
+			},
+			{ key = "LeftArrow", mods = "NONE", action = act.CopyMode("MoveLeft") },
+			{
+				key = "LeftArrow",
+				mods = "ALT",
+				action = act.CopyMode("MoveBackwardWord"),
+			},
+			{
+				key = "RightArrow",
+				mods = "NONE",
+				action = act.CopyMode("MoveRight"),
+			},
+			{
+				key = "RightArrow",
+				mods = "ALT",
+				action = act.CopyMode("MoveForwardWord"),
+			},
+			{ key = "UpArrow", mods = "NONE", action = act.CopyMode("MoveUp") },
+			{ key = "DownArrow", mods = "NONE", action = act.CopyMode("MoveDown") },
+		},
+	}
 end
 
 function M.smart_nav(resize_or_move, mods, key, dir)
