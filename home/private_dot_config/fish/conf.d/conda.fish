53fc6bb3442714547341495eadb9a768d89020a6
Johnson Hu <huwqchn@gmail.com>
2023-11-16 10:28:43 +0800

    feat(fish): add conda init

---
+++ home/private_dot_config/fish/conf.d/conda.fish
@@ -0,0 +1,13 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda/bin/conda
+    eval /opt/anaconda/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/opt/anaconda/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /opt/anaconda/bin $PATH
+    end
+end
+# <<< conda initialize <<<
+export TERMINFO=/usr/share/terminfo
