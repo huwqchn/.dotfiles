e2ec035392e4289f0ae612aac9e26d65482a89d2
Johnson Hu <huwqchn@gmail.com>
2023-12-14 10:09:20 +0800

    feat(nvim): add text-case plugin

---
+++ home/private_dot_config/nvim/lua/plugins/extras/misc/text-case.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({})
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "v" }, desc = "Telescope" },
+    },
+  },
+}
