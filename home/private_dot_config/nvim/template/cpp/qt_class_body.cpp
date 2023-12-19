c2926d3e55224dbad2ed352b389304bca113c88d
Johnson Hu <huwqchn@gmail.com>
2023-12-19 17:54:31 +0800

    feat(nvim): add template plugin and template file

---
+++ home/private_dot_config/nvim/template/cpp/qt_class_body.cpp
@@ -0,0 +1,8 @@
+// Copyright {{_date_}} {{_author_}}. All rights reserved.
+#include "{{_file_name_}}.h"
+
+{{_file_name_}}::{{_file_name_}}(QWidget *parent) : QWidget(parent) {}
+
+{{_file_name_}}::~{{_file_name_}}() {}
+
+{{_cursor_}}
---
+++ home/private_dot_config/nvim/template/cpp/qt_class_header.cpp
@@ -0,0 +1,15 @@
+// Copyright {{_date_}} {{_author_}}. All rights reserved.
+#ifndef {{_upper_file_}}
+#define {{_upper_file_}}
+#include <QWidget>
+
+class {{_file_name_}} : public QWidget {
+  Q_OBJECT
+
+public:
+  {{_file_name_}}(QWidget *parent = nullptr);
+  ~{{_file_name_}}();
+  {{_cursor_}}
+};
+
+#endif // {{_upper_file_}}
