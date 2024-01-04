b6943490f7c83b6a10c3c55faeb415822e115deb
Johnson Hu <huwqchn@gmail.com>
2024-01-04 17:27:49 +0800

    feat(nvim): tabout

--- home/private_dot_config/nvim/lua/plugins/extras/coding/complete/supertab.lua
+++ home/private_dot_config/nvim/lua/plugins/extras/coding/complete/supertab.lua
@@ -1,5 +1,61 @@
 return {
   {
+    "kawre/neotab.nvim",
+    event = "InsertEnter",
+    opts = {
+      tabkey = "",
+      act_as_tab = true,
+      behavior = "nested",
+      pairs = {
+        { open = "(", close = ")" },
+        { open = "[", close = "]" },
+        { open = "{", close = "}" },
+        { open = "'", close = "'" },
+        { open = '"', close = '"' },
+        { open = "`", close = "`" },
+        { open = "<", close = ">" },
+      },
+      exclude = {},
+      smart_punctuators = {
+        enabled = false,
+        semicolon = {
+          enabled = false,
+          ft = { "cs", "c", "cpp", "java" },
+        },
+        escape = {
+          enabled = true,
+          triggers = {
+            ["+"] = {
+              pairs = {
+                { open = '"', close = '"' },
+              },
+              space = { before = true, after = true },
+              -- string.format(format, typed_char)
+              format = " %s ", -- " + "
+              ft = { "java" },
+            },
+            [","] = {
+              pairs = {
+                { open = "'", close = "'" },
+                { open = '"', close = '"' },
+              },
+              format = "%s ", -- ", "
+            },
+            ["="] = {
+              pairs = {
+                { open = "(", close = ")" },
+              },
+              ft = { "javascript", "typescript" },
+              format = " %s> ", -- ` => `
+              -- string.match(text_between_pairs, cond)
+              cond = "^$", -- match only pairs with empty content
+            },
+          },
+        },
+      },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
@@ -104,6 +160,7 @@ return {
 
       local luasnip = require("luasnip")
       local cmp = require("cmp")
+      local neotab = require("neotab")
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
@@ -115,10 +172,11 @@ return {
             luasnip.expand_or_jump()
           elseif jumpable(1) then
             luasnip.jump(1)
-          elseif has_words_before() then
-            cmp.complete()
+          -- elseif has_words_before() then
+          --   cmp.complete()
           else
-            fallback()
+            -- fallback()
+            neotab.tabout()
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
