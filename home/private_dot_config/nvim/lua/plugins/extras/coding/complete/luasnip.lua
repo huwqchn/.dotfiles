b29a1e9cc4ac7a4c5cfe1508ba33ac74fdb7e0fb
Johnson Hu <huwqchn@gmail.com>
2024-01-10 10:26:51 +0800

    feat(nvim): add edit luasnip plugin

--- home/private_dot_config/nvim/lua/plugins/extras/coding/complete/luasnip.lua
+++ home/private_dot_config/nvim/lua/plugins/extras/coding/complete/luasnip.lua
@@ -51,4 +51,27 @@ return {
       _G.conds_expand = require("luasnip.extras.conditions.expand")
     end,
   },
+  {
+    "chrisgrieser/nvim-scissors",
+    dependencies = "nvim-telescope/telescope.nvim", -- optional
+    opts = {
+      snippetDir = "~/.config/nvim/snippets/",
+    },
+    keys = {
+      {
+        "<leader>ce",
+        function()
+          require("scissors").editSnippet()
+        end,
+        desc = "edit snippet",
+      },
+      {
+        "<leader>a",
+        function()
+          require("scissors").addNewSnippet()
+        end,
+        desc = "add new snippet",
+      },
+    },
+  },
 }
