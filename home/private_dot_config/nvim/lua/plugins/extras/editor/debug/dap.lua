0227e085357dc56c9b1f99bb2964ff3f0410880d
Johnson Hu <huwqchn@gmail.com>
2024-01-12 15:39:46 +0800

    fix(nvim): fix nvim dap keymaps changes

--- home/private_dot_config/nvim/lua/plugins/extras/editor/debug/dap.lua
+++ home/private_dot_config/nvim/lua/plugins/extras/editor/debug/dap.lua
@@ -1,47 +1,76 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    keys = function(_, keys)
-      for _, key in ipairs(keys) do
-        if key[1] == "<leader>dj" then
-          key[1] = "<leader>de"
-        elseif key[1] == "<leader>dk" then
-          key[1] = "<leader>di"
-        elseif key[1] == "<leader>di" then
-          key[1] = "<leader>dI"
-        elseif key[1] == "<leader>db" then
-          key[1] = "<leader>dd"
-        elseif key[1] == "<leader>dB" then
-          key[1] = "<leader>dD"
-        end
-      end
-      keys = vim.tbl_extend("force", keys, {
-        {
-          "<leader>db",
-          function()
-            require("dap").step_back()
-          end,
-          desc = "Step back",
-        },
-        -- jetbrains-like debug keymaps
-        { "<F7>", "<leader>dI", desc = "Step Into", remap = true },
-        { "<S-F7>", "<leader>do", desc = "Step Out", remap = true },
-        { "<F8>", "<leader>dO", desc = "Step Over", remap = true },
-        { "<S-F8>", "<leader>db", desc = "Step Back", remap = true },
-        { "<F9>", "<leader>dC", desc = "Run to Cursor", remap = true },
-        { "<F10>", "<leader>dc", desc = "Continue", remap = true },
-      })
-    end,
+    keys = {
+      { "<leader>dj", false },
+      { "<leader>dk", false },
+      { "<leader>dB", false },
+
+      {
+        "<leader>db",
+        function()
+          require("dap").step_back()
+        end,
+        desc = "Step back",
+      },
+      {
+        "<leader>de",
+        function()
+          require("dap").down()
+        end,
+        desc = "Down",
+      },
+      {
+        "<leader>di",
+        function()
+          require("dap").up()
+        end,
+        desc = "Up",
+      },
+
+      {
+        "<leader>dI",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<leader>dd",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle Breakpoint",
+      },
+      {
+        "<leader>dD",
+        function()
+          require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end,
+        desc = "Breakpoint Condition",
+      },
+      -- jetbrains-like debug keymaps
+      { "<F7>", "<leader>dI", desc = "Step Into", remap = true },
+      { "<S-F7>", "<leader>do", desc = "Step Out", remap = true },
+      { "<F8>", "<leader>dO", desc = "Step Over", remap = true },
+      { "<S-F8>", "<leader>db", desc = "Step Back", remap = true },
+      { "<F9>", "<leader>dC", desc = "Run to Cursor", remap = true },
+      { "<F10>", "<leader>dc", desc = "Continue", remap = true },
+    },
   },
 
   {
     "rcarriga/nvim-dap-ui",
-    keys = function(_, keys)
-      for _, key in ipairs(keys) do
-        if key[1] == "<leader>de" then
-          key[1] = "<leader>dE"
-        end
-      end
-    end,
+    keys = {
+      { "<leader>de", false, mode = { "n", "v" } },
+      {
+        "<leader>dE",
+        function()
+          require("dapui").eval()
+        end,
+        desc = "Eval",
+        mode = { "n", "v" },
+      },
+    },
   },
 }
