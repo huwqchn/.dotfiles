1ad4c1190186c9d3040bb22d8f074fb84bfc2521
Johnson Hu <huwqchn@gmail.com>
2024-01-23 19:10:34 +0800

    feat(fish): add nnU-Net env

--- home/private_dot_config/fish/conf.d/nnUNet.fish
+++ home/private_dot_config/fish/conf.d/nnUNet.fish
@@ -1,4 +1,3 @@
-set -x LD_LIBRARY_PATH $HOME/.conda/envs/nnUnet/lib
 set -x nnUNet_raw $HOME/Repos/dl-repos/nnUNet/nnUNetFrame/DATASET/nnUNet_raw
 set -x nnUNet_results $HOME/Repos/dl-repos/nnUNet/nnUNetFrame/DATASET/nnUNet_results
 set -x nnUNet_preprocessed $HOME/Repos/dl-repos/nnUNet/nnUNetFrame/DATASET/nnUNet_preprocessed
