87b9802c9749c91b2158683a358816a020de9d13
Johnson Hu <huwqchn@gmail.com>
2024-04-01 11:59:55 +0800

    feat: rm unused png

--- home/private_dot_config/nvim/lua/plugins/extras/editor/keymaps/which-key.lua
+++ home/private_dot_config/nvim/lua/plugins/extras/editor/keymaps/which-key.lua
@@ -2,25 +2,25 @@ return {
   {
     "folke/which-key.nvim",
     optional = true,
-    dependencies = {
-      {
-        "mrjones2014/legendary.nvim",
-        -- version = "v2.1.0",
-        -- since legendary.nvim handles all your keymaps/commands,
-        -- its recommended to load legendary.nvim before other plugins
-        priority = 10000,
-        lazy = false,
-        opts = {
-          extensions = {
-            lazy_nvim = { auto_register = true },
-          },
-        },
-        keys = {
-          { "<leader>h", "<cmd>Legendary<cr>", mode = { "n", "v" } },
-          { "<leader>.", "<cmd>LegendaryRepeat<cr>", mode = { "n", "v" } },
-        },
-      },
-    },
+    -- dependencies = {
+    --   {
+    --     "mrjones2014/legendary.nvim",
+    --     -- version = "v2.1.0",
+    --     -- since legendary.nvim handles all your keymaps/commands,
+    --     -- its recommended to load legendary.nvim before other plugins
+    --     priority = 10000,
+    --     lazy = false,
+    --     opts = {
+    --       extensions = {
+    --         lazy_nvim = { auto_register = true },
+    --       },
+    --     },
+    --     keys = {
+    --       { "<leader>h", "<cmd>Legendary<cr>", mode = { "n", "v" } },
+    --       { "<leader>.", "<cmd>LegendaryRepeat<cr>", mode = { "n", "v" } },
+    --     },
+    --   },
+    -- },
     keys = {
       { "<leader>", mode = { "n", "v" } },
       { "g", mode = { "n", "v" } },
