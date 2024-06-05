4cda3bba5c9460bc42fceccec4d3d5ace8aa12cf
Johnson Hu <huwqchn@gmail.com>
2024-06-05 15:24:34 +0800

    feat(nvim): update dropbar keymaps

--- home/private_dot_config/nvim/lua/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/plugins/ui.lua
@@ -14,7 +14,51 @@ return {
         desc = "Select dropbar",
       },
     },
-    opts = true,
+    opts = function()
+      local menu_utils = require("dropbar.utils.menu")
+
+      -- Closes all the windows in the current dropbar.
+      local function close()
+        local menu = menu_utils.get_current()
+        while menu and menu.prev_menu do
+          menu = menu.prev_menu
+        end
+        if menu then
+          menu:close()
+        end
+      end
+
+      return {
+        menu = {
+          keymaps = {
+            -- Navigate back to the parent menu.
+            ["n"] = "<C-w>q",
+            -- Expands the entry if possible.
+            ["o"] = function()
+              local menu = menu_utils.get_current()
+              if not menu then
+                return
+              end
+              local row = vim.api.nvim_win_get_cursor(menu.win)[1]
+              local component = menu.entries[row]:first_clickable()
+              if component then
+                menu:click_on(component, nil, 1, "l")
+              end
+            end,
+            ["O"] = function()
+              local menu = menu_utils.get_current()
+              if not menu then
+                return
+              end
+              menu:fuzzy_find_open()
+            end,
+            ["i"] = "k", -- don't use i for fuzzy search, use O instead
+            ["q"] = close,
+            ["<esc>"] = close,
+          },
+        },
+      }
+    end,
     event = "VeryLazy",
   },
   {
