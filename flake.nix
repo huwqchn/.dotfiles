aadbeacc4ffeea20dac01eb760674ed18ff67b66
Johnson Hu <huwqchn@gmail.com>
2024-07-28 22:00:27 +0800

    feat: change outputs to systems

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "my NixOS flakes configuration";
 
+  outputs = inputs: import ./systems inputs;
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
@@ -83,39 +85,10 @@
       url = "gitlab:VandalByte/dedsec-grub-theme";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
-  outputs = { self, nixpkgs, home-manager, nix-darwin, dedsec-grub-theme, ... } @ inputs:
-  let
-    inherit (inputs.nixpkgs) lib;
-    mylib = import ../lib { inherit lib; };
-    myvars = import ./vars { inherit lib; };
-    specialArgs = {
-      inherit inputs mylib myvars;
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixosSystem = "x86_64-linux";
-    darwinSystem = "aarch64-darwin";
-  in {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = nixosSystem;
-      inherit specialArgs;
-      modules = [
-        dedsec-grub-theme.nixosModule
-        ./nixos/configuration.nix
-        home-manager.nixosModules.home-manager
-      ];
-    };
-
-    darwinConfigurations.dailydev = nix-darwin.lib.darwinSystem {
-      system = darwinSystem;
-      inherit specialArgs;
-      modules = [
-        ./darwin/configuration.nix
-        home-manager.darwinModules.home-manager
-      ];
-    };
-    # nix code formatter
-    formatter.${darwinSystem} = nixpkgs.legacyPackages.${darwinSystem}.alejandra;
-    formatter.${nixosSystem} = nixpkgs.legacyPackages.${nixosSystem}.alejandra;
   };
 }
