8cacf584adaaec2638f181822fb914e6e3c266db
Johnson Hu <huwqchn@gmail.com>
2024-08-06 17:17:18 +0800

    fix: another typos

--- home/linux/gui/fcitx5.nix
+++ home/linux/gui/fcitx5.nix
@@ -13,7 +13,7 @@
 
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addon = with pkgs; [
+    fcitx5.addons = with pkgs; [
       # for flypy chinese input method
       fcitx5-rime
       # needed enable rime using configtool after installed
