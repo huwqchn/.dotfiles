7934c4c7c94039a203e1bb81410aebb03f52109a
Johnson Hu <huwqchn@gmail.com>
2024-08-07 12:47:39 +0800

    feat: change host modules for test

--- systems/x86_64-linux/src/hacker.nix
+++ systems/x86_64-linux/src/hacker.nix
@@ -12,36 +12,25 @@
 } @ args: let
   name = "hacker";
   modules = {
-    nixos-modules = 
+    nixos-modules =
       (map mylib.relativeToRoot [
         "hosts/${name}"
         "modules/base.nix"
-        "modules/nixos/base"
-        "modules/nixos/desktop"
-        # "modules/nixos/desktop/game"
-        # "modules/nixos/desktop/fhs.nix"
-        # "modules/nixos/desktop/fonts.nix"
-        # "modules/nixos/desktop/graphic.nix"
-        # "modules/nixos/desktop/guix.nix"
-        # "modules/nixos/desktop/misc.nix"
-        # "modules/nixos/desktop/peripherals.nix"
-        # "modules/nixos/desktop/security.nix"
-        # "modules/nixos/desktop/virtualisation.nix"
-        # "modules/nixos/desktop/wayland.nix"
-        "modules/nixos/desktop.nix"
+        "modules/base"
+        "modules/desktop"
+        "modules/desktop.nix"
       ])
       ++ [
         {
           modules.desktop.wayland.enable = true;
         }
       ];
-    home-modules = 
+    home-modules =
       (map mylib.relativeToRoot [
-
         "hosts/${name}/home.nix"
         "home/base"
         "home/linux"
-      ]) 
+      ])
       ++ [
         {
           modules.desktop.hyprland.enable = true;
