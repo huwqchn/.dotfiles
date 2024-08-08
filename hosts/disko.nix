ee2548bd389fd712e689993354f7139d544eeef0
Johnson Hu <huwqchn@gmail.com>
2024-08-09 00:56:34 +0800

    feat: add disko function

---
+++ hosts/disko.nix
@@ -0,0 +1,80 @@
+# NOTE: ... is needed because dikso passes diskoFile
+{ lib
+, disk ? throw "Set this to your disk device, e.g. /dev/sda",
+, withSwap ? false
+, swapSize
+, configVars
+, ...
+}:
+{
+  disko = {
+    # Do not let Disko manage fileSystems.* config for NixOS.
+    # Reason is that Disko mounts partitions by GPT partition names, which are
+    # easily overwritten with tools like fdisk. When you fail to deploy a new
+    # config in this case, the old config that comes with the disk image will
+    # not boot either.
+    enableConfig = false;
+    devices = {
+        disk = {
+        disk0 = {
+            type = "disk";
+            device = disk;
+            content = {
+            type = "gpt";
+            partitions = {
+                ESP = {
+                priority = 1;
+                name = "ESP";
+                start = "1M";
+                end = "512M";
+                type = "EF00";
+                content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "defaults" ];
+                };
+                };
+                root = {
+                size = "100%";
+                content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ]; # Override existing partition
+                    # Subvolumes must set a mountpoint in order to be mounted,
+                    # unless their parent is mounted
+                    subvolumes = {
+                    "@root" = {
+                        mountpoint = "/";
+                        mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        ];
+                    };
+                    "@persist" = {
+                        mountpoint = "${configVars.persistFolder}";
+                        mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        ];
+                    };
+                    "@nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                        ];
+                    };
+                    "@swap" = lib.mkIf withSwap {
+                        mountpoint = "/.swapvol";
+                        swap.swapfile.size = "${swapSize}G";
+                    };
+                    };
+                };
+                };
+            };
+            };
+        };
+        };
+    };
+  };
+}
