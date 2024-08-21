e3c74fc7a16d776b53b3fd65c38c52a7c6d52ac1
Johnson Hu <huwqchn@gmail.com>
2024-08-22 00:57:45 +0800

    feat: update powershell config

--- config/powershell.ps1
+++ config/powershell.ps1
@@ -4,7 +4,75 @@ Invoke-Expression (&starship init powershell)
 Set-Alias -Name v -Value nvim
 Set-Alias -Name g -Value git
 Set-Alias -Name lg -Value lazygit
-Set-Alias -Name y -Value yazi
+#Set-Alias -Name y -Value yazi
+# yazi
+function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
+# eza aliases
+Set-Alias -Name ls -Value new_ls
+Set-Alias -Name la -Value new_la
+Set-Alias -Name lsa -Value new_la
+Set-Alias -Name ll -Value new_ll
+Set-Alias -Name lla -Value new_lla
+Set-Alias -Name lah -Value new_lah
+
+function new_ls { eza --color=always --icons=always --group-directories-first @args }
+function new_la { eza -a --color=always --icons=always --group-directories-first @args }
+function new_ll { eza -l --no-permissions --no-filesize --no-time --no-user --color=always --icons=always --group-directories-first @args }
+function new_lla { eza -la --no-permissions --no-filesize --no-time --no-user --color=always --icons=always --group-directories-first @args }
+function new_lah { eza -lah --color=always --icons=always --group-directories-first @args }
+
+Set-Alias -Name tree -Value new_tree
+Set-Alias -Name treea -Value new_treea
+Set-Alias -Name treel -Value new_treel
+Set-Alias -Name treela -Value new_treela
+
+function new_tree { eza --tree --level=2 --color=always --icons=always --group-directories-first @args }
+function new_treea { eza --tree -a --level=2 --color=always --icons=always --group-directories-first @args }
+function new_treel { eza --tree --color=always --icons=always --group-directories-first @args }
+function new_treela { eza --tree -a --color=always --icons=always --group-directories-first @args }
+# Fzf
+# FZF config
+# Enable fd integration
+$env:FZF_DEFAULT_COMMAND='fd --type=f --strip-cwd-prefix --hidden --follow --exclude .git'
+$env:FZF_CTRL_T_COMMAND=$env:FZF_DEFAULT_COMMAND
+$env:FZF_ALT_C_COMMAND='fd --type=d --strip-cwd-prefix --hidden --follow --exclude .git'
+# Use bat to show file preview
+$env:FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+$env:FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {}'"
+# Customise the look of fzf
+
+$env:FZF_DEFAULT_OPTS="
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#2d3f76 \
+  --color=bg:#1e2030 \
+  --color=border:#589ed7 \
+  --color=fg:#c8d3f5 \
+  --color=gutter:#1e2030 \
+  --color=header:#ff966c \
+  --color=hl+:#65bcff \
+  --color=hl:#65bcff \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#65bcff \
+  --color=query:#c8d3f5:regular \
+  --color=scrollbar:#589ed7 \
+  --color=separator:#ff966c \
+  --color=spinner:#ff007c \
+  "
+
 # prompt
 $prompt = ""
 function Invoke-Starship-PreCommand {
