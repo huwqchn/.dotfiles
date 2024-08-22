b28987db886e972aa3b6a47124d6479fdc8cae90
Johnson Hu <huwqchn@gmail.com>
2024-08-22 16:01:14 +0800

    fix: rm fzf options

--- config/powershell.ps1
+++ config/powershell.ps1
@@ -41,37 +41,37 @@ function new_treela { eza --tree -a --color=always --icons=always --group-direct
 # Fzf
 # FZF config
 # Enable fd integration
-$env:FZF_DEFAULT_COMMAND='fd --type=f --strip-cwd-prefix --hidden --follow --exclude .git'
-$env:FZF_CTRL_T_COMMAND=$env:FZF_DEFAULT_COMMAND
-$env:FZF_ALT_C_COMMAND='fd --type=d --strip-cwd-prefix --hidden --follow --exclude .git'
+#$env:FZF_DEFAULT_COMMAND='fd --type=f --strip-cwd-prefix --hidden --follow --exclude .git'
+#$env:FZF_CTRL_T_COMMAND=$env:FZF_DEFAULT_COMMAND
+#$env:FZF_ALT_C_COMMAND='fd --type=d --strip-cwd-prefix --hidden --follow --exclude .git'
 # Use bat to show file preview
-$env:FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
-$env:FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {}'"
+#$env:FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+#$env:FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {}'"
 # Customise the look of fzf
 
-$env:FZF_DEFAULT_OPTS="
-  --highlight-line \
-  --info=inline-right \
-  --ansi \
-  --layout=reverse \
-  --border=none \
-  --color=bg+:#2d3f76 \
-  --color=bg:#1e2030 \
-  --color=border:#589ed7 \
-  --color=fg:#c8d3f5 \
-  --color=gutter:#1e2030 \
-  --color=header:#ff966c \
-  --color=hl+:#65bcff \
-  --color=hl:#65bcff \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#65bcff \
-  --color=query:#c8d3f5:regular \
-  --color=scrollbar:#589ed7 \
-  --color=separator:#ff966c \
-  --color=spinner:#ff007c \
-  "
+#$env:FZF_DEFAULT_OPTS="
+#  --highlight-line \
+#  --info=inline-right \
+#  --ansi \
+#  --layout=reverse \
+#  --border=none \
+#  --color=bg+:#2d3f76 \
+#  --color=bg:#1e2030 \
+#  --color=border:#589ed7 \
+#  --color=fg:#c8d3f5 \
+#  --color=gutter:#1e2030 \
+#  --color=header:#ff966c \
+#  --color=hl+:#65bcff \
+#  --color=hl:#65bcff \
+#  --color=info:#545c7e \
+#  --color=marker:#ff007c \
+#  --color=pointer:#ff007c \
+#  --color=prompt:#65bcff \
+#  --color=query:#c8d3f5:regular \
+#  --color=scrollbar:#589ed7 \
+#  --color=separator:#ff966c \
+#  --color=spinner:#ff007c \
+#  "
 
 # prompt
 $prompt = ""
