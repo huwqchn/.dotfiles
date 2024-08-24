c09180ec1f61f82985adb268c1e5345e54169d97
Johnson Hu <johnson.wq.hu@gmail.com>
2024-08-25 05:55:38 +0800

    fix: disable dev-tpmrm0.device cause it timed out on boot

--- hosts/oldman/__config/default.nix
+++ hosts/oldman/__config/default.nix
@@ -20,6 +20,9 @@ in {
       # ../../_internal/lanzaboote.nix
     ];
 
+  # fix timed out for device /dev/tpmrm0
+  systemd.units."dev-tpmrm0.device".enable = false;
+
   # Use the systemd-boot EFI boot loader.
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
