170345e283042e3492b5f9d5f648e6de42e03dcd
Johnson Hu <huwqchn@gmail.com>
2024-08-31 23:15:45 +0800

    feat: add simple luks-btrfs-tmpfs disko config

---
+++ hosts/_internal/disko/luks-btrfs-tmpfs.nix
@@ -0,0 +1,79 @@
+{
+  device ?  "/dev/nvme0n1",
+  swapSize ? "32G",
+  ...
+}:
+{
+  disko.devices = {
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "defaults"
+        "size=8G"
+        "mode=755"
+      ];
+    };
+
+    disk.main = {
+      type = "disk";
+      inherit device;
+
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted";
+              settings = {
+                allowDiscards = true;
+              };
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  nix = {
+                    mountpoint = "/nix";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  persist = {
+                    mountpoint = "/persist";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  log = {
+                    mountpoint = "/var/log";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  tmp = {
+                    mountpoint = "/tmp";
+                    mountOptions = ["noatime"];
+                  };
+                  swap = {
+                    mountpoint = "/swap";
+                    mountOptions = ["noatime"];
+                    swap.swapfile.size = swapSize;
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
