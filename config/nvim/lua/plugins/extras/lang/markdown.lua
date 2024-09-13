fc7e580a40af1ce15047afc923f4d6fb149c23d3
Johnson Hu <johnson.wq.hu@gmail.com>
2024-09-13 16:59:30 +0800

    fix(nvim): markdown image render error messages

--- config/nvim/lua/plugins/extras/lang/markdown.lua
+++ config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -10,4 +10,20 @@ return {
     config = true,
     cmd = { "Maim", "MaimMarkdown" },
   },
+  {
+    "3rd/image.nvim",
+    enabled = not vim.g.neovide,
+    ft = { "markdown", "vimwiki", "quarto", "python" },
+    opts = {
+      tmux_show_only_in_active_window = true,
+      integrations = {
+        markdown = {
+          only_render_image_at_cursor = true,
+          only_render_image_at_cursor_mode = "popup",
+          filetypes = { "markdown", "vimwiki", "quarto", "python" },
+        },
+      },
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif", "*.svg" },
+    },
+  },
 }
