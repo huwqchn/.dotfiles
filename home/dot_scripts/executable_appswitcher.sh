c10e59adf3d97ece2d8aae7a34c626d0c6671061
Johnson Hu <huwqchn@gmail.com>
2024-09-13 23:26:11 +0800

    feat: big cleanup for nixos

--- home/dot_scripts/executable_appswitcher.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rofi -show window -icon-theme "Papirus" -show-icons -hover-select -me-select-entry '' -me-accept-entry MousePrimary
-
--- home/private_dot_config/fish/conf.d/nnUNet.fish
+++
@@ -1,3 +0,0 @@
-set -x nnUNet_raw $HOME/Extend/nnUNet/raw
-set -x nnUNet_results $HOME/Extend/nnUNet/results
-set -x nnUNet_preprocessed $HOME/Extend/nnUNet/preprocessed
