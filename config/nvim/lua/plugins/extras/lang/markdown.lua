b0812200c96cac2f582e2c095f847fc88f0bd49e
Johnson Hu <johnson.wq.hu@gmail.com>
2024-09-19 20:45:39 +0800

    feat(nvim): update lazyvim

--- config/nvim/lua/plugins/extras/lang/markdown.lua
+++ config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -1,109 +1,12 @@
 return {
-  -- { import = "lazyvim.plugins.extras.lang.markdown" },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters = {
-        ["markdown-toc"] = {
-          condition = function(_, ctx)
-            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-              if line:find("<!%-%- toc %-%->") then
-                return true
-              end
-            end
-          end,
-        },
-        ["markdownlint-cli2"] = {
-          condition = function(_, ctx)
-            local diag = vim.tbl_filter(function(d)
-              return d.source == "markdownlint"
-            end, vim.diagnostic.get(ctx.buf))
-            return #diag > 0
-          end,
-        },
-      },
-      formatters_by_ft = {
-        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = { ensure_installed = { "markdownlint-cli2", "markdown-toc" } },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.diagnostics.markdownlint_cli2,
-      })
-    end,
-  },
-  {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        markdown = { "markdownlint-cli2" },
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        marksman = {},
-      },
-    },
-  },
-
-  -- Markdown preview
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-      vim.fn["mkdp#util#install"]()
-    end,
-    keys = {
-      {
-        "<leader>cp",
-        ft = "markdown",
-        "<cmd>MarkdownPreviewToggle<cr>",
-        desc = "Markdown Preview",
-      },
-    },
-    config = function()
-      vim.cmd([[do FileType]])
-    end,
-  },
-
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown", "vimwiki", "norg", "rmd", "org" },
-    opts = {
-      file_types = { "markdown", "norg", "rmd", "org" },
-    },
-    config = function(_, opts)
-      require("render-markdown").setup(opts)
-      LazyVim.toggle.map("<leader>um", {
-        name = "Render Markdown",
-        get = function()
-          return require("render-markdown.state").enabled
-        end,
-        set = function(enabled)
-          local m = require("render-markdown")
-          if enabled then
-            m.enable()
-          else
-            m.disable()
-          end
-        end,
-      })
+    opts = function()
+      return {
+        file_types = { "markdown", "norg", "rmd", "org" },
+      }
     end,
   },
   {
