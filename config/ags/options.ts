9d3905eb9d936e81202fada5f8027ea20917069e
Johnson Hu <johnson.wq.hu@gmail.com>
2024-09-26 23:32:28 +0800

    feat(ags): update date format

--- config/ags/options.ts
+++ config/ags/options.ts
@@ -95,7 +95,7 @@ const options = mkOptions(OPTIONS, {
       action: opt(() => App.toggleWindow("launcher")),
     },
     date: {
-      format: opt("%H:%M - %A %e."),
+      format: opt("%H:%M - %A"),
       action: opt(() => App.toggleWindow("datemenu")),
     },
     battery: {
