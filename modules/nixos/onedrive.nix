753b418798b534c102a249a5d4b731830a80cfe9
Johnson Hu <johnson.wq.hu@gmail.com>
2024-10-19 13:26:11 +0800

    feat: add onedrive

---
+++ modules/nixos/onedrive.nix
@@ -0,0 +1,16 @@
+{myvars, ...}: {
+  services.onedrive.enable = true;
+
+  home-manager.users.${myvars.userName} = {
+    xdg.configFile."onedrive/config".text = ''
+      # try to download changes from onedrive every x seconds
+      monitor_interval = "6"
+      # fully scan data fro integrity every x attempt of download
+      monitor_fullscan_frequency = "50"
+      # minimum number of downloaded changes to trigger desktop notification
+      min_notify_changes = "1"
+      # ignore temporary stuff and weird obsidian file
+      skip_file = "~*|.~*|*.tmp|.OBSIDIANTEST"
+    '';
+  };
+}
