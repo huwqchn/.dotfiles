0fe0a1d2cbc6207ec1d40769cd75688462763ef0
Johnson Hu <johnson.wq.hu@gmail.com>
2024-10-19 22:52:22 +0800

    refactor: make network module

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,9 +1,20 @@
 {
   config,
+  pkgs,
   mylib,
   ...
 }:
 mylib.mkEnabledModule config "network" {
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openconnect
+      networkmanager-l2tp
+    ];
+  };
+
+  programs.nm-applet.enable = true;
+
   # Network discovery, mDNS, DNS-SD
   # With this enabled, you can access your machine at <hostname>.local
   # it's more convenient than using IP addresses
