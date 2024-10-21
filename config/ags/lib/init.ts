cf3857154e098a636c17ef44c93033a01169295c
Johnson Hu <johnson.wq.hu@gmail.com>
2024-10-22 01:04:35 +0800

    feat(ags): rm tmux init

--- config/ags/lib/init.ts
+++ config/ags/lib/init.ts
@@ -1,6 +1,6 @@
 import matugen from "./matugen"
 import hyprland from "./hyprland"
-import tmux from "./tmux"
+// import tmux from "./tmux"
 import gtk from "./gtk"
 import lowBattery from "./battery"
 import notifications from "./notifications"
@@ -8,7 +8,7 @@ import notifications from "./notifications"
 export default function init() {
   try {
     gtk()
-    tmux()
+    // tmux()
     matugen()
     lowBattery()
     notifications()
