c6e89e827d6a6316b66c7f2092faac8e88e706e5
Johnson Hu <johnson.wq.hu@gmail.com>
2025-01-04 02:06:36 +0800

    fix: missing parameter

--- modules/nixos/atuin-autosync.nix
+++ modules/nixos/atuin-autosync.nix
@@ -1,9 +1,14 @@
-{mylib, pkgs, ...}:
+{
+  mylib,
+  pkgs,
+  config,
+  ...
+}:
 mylib.mkModule config "atuin-autosync" {
   systemd.user.timers.atuin-sync = {
     Unit.Description = "Atuin auto sync";
     Timer.OnUnitActiveSec = "1h";
-    Install.WantedBy = [ "timers.target" ];
+    Install.WantedBy = ["timers.target"];
   };
 
   systemd.user.services.atuin-sync = {
