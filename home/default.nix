ee4e1119c45a14ef9b485bce640f356aac6528e1
Wenqaing Hu <huwqchn@gmail.com>
2025-03-04 18:13:36 +0800

    feat: alias home-manager as hm

--- home/default.nix
+++ home/default.nix
@@ -1,1 +1,25 @@
-{lib, ...}: {imports = lib.my.scanPaths ./.;}
+{
+  inputs,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    (lib.mkAliasOptionModule [ "hm" ] [
+      "home-manager"
+      "users"
+      config.my.name
+    ])
+  ];
+
+  hm.imports = lib.my.scanPaths ./.;
+
+  home-manager.extraSpecialArgs = {
+    inherit inputs;
+  };
+  home-manager.useGlobalPkgs = true;
+  # do not enable home-manager.useUserPackages, to match standalone home-manager,
+  # so home-manager/nixos-rebuild/darwin-rebuild can be used at the same time
+  # home-manager.useUserPackages = true;
+}
