97c1b5feceac67f67ff5a63425e9b3b58e7fe83f
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-06 05:21:24 +0800

    feat: change lazyvim nix formatter as same with nix config

--- home/editors/nvim/lazyvim/plugins/lang/nix.nix
+++ home/editors/nvim/lazyvim/plugins/lang/nix.nix
@@ -7,22 +7,17 @@
   inherit (lib) mkEnableOption mkIf;
   cfg = config.my.neovim.lazyvim.nix;
 in {
-  options.my.neovim.lazyvim.nix = {
-    enable = mkEnableOption "language nix";
-  };
+  options.my.neovim.lazyvim.nix = {enable = mkEnableOption "language nix";};
 
   config = mkIf cfg.enable {
-    programs.neovim.extraPackages = with pkgs; [
-      nil
-      nixfmt-classic
-    ];
+    programs.neovim.extraPackages = with pkgs; [nil alejandra];
 
-    my.neovim.treesitterParsers = [
-      "nix"
-    ];
+    my.neovim.treesitterParsers = ["nix"];
 
-    my.neovim.lazyvim.extraSpec = ''
-      { import = "lazyvim.plugins.extras.lang.nix" },
-    '';
+    # my.neovim.lazyvim.extraSpec = ''
+    #   { import = "lazyvim.plugins.extras.lang.nix" },
+    # '';
+    xdg.configFile."nvim/lua/plugins/nix.lua".source =
+      lib.my.relativeToConfig "nvim/lua/plugins/extras/lang/nix.lua";
   };
 }
