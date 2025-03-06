763e998d699975ee41cdad57bf4658ca5b2f5d6f
Wenqaing Hu <huwqchn@gmail.com>
2025-03-06 10:16:57 +0800

    feat: add more git aliases

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -46,22 +46,79 @@ lib.my.mkEnabledModule config "git" {
       };
     };
 
-    ignores = [".*.sw?" ".direnv/" ".envrc"];
+    ignores = [".*.sw?" ".direnv/" ".envrc" "result*" "node_modules"];
 
-    aliases = {
+    aliases = let
+      log = "log --show-notes='*' --abbrev-commit --pretty=format:'%Cred%h %Cgreen(%aD)%Creset -%C(bold red)%d%Creset %s %C(bold blue)<%an>% %Creset' --graph";
+    in {
       # common aliases
-      br = "branch";
+      # add
+      a = "add --patch";
+      ad = "add";
+
+      # branch
+      b = "branch";
+      ba = "branch -a"; # list remote branches
+      bd = "branch --delete";
+      bD = "branch -D";
+
+      # commit
+      c = "commit";
+      ca = "commit --amend";
+      cm = "commit --message";
+
       co = "checkout";
-      st = "status";
-      ls = ''
-        log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'';
-      ll = ''
-        log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'';
-      cm = "commit -m";
-      ca = "commit -am";
+      cb = "checkout -b";
+      pc = "checkout --patch";
+
+      cl = "clone";
+
+      # diff
+      d = "diff";
+      ds = "diff --staged";
       dc = "diff --cached";
-      amend = "commit --amend -m";
+
+      # show
+      h = "show";
+      h1 = "show HEAD^";
+      h2 = "show HEAD^^";
+      h3 = "show HEAD^^^";
+      h4 = "show HEAD^^^^";
+      h5 = "show HEAD^^^^^";
+
+      # push & pull
+      P = "push";
+      Pf = "push --force-with-lease";
+      p = "pull";
       pr = "pull --rebase";
+      # rebase
+      r = "rebase";
+      ra = "rebase --abort";
+      rc = "rebase --continue";
+      ri = "rebase --interactive";
+      # reset
+      R = "reset";
+      Rh = "reset --hard";
+
+      # log
+      l = log;
+      la = "${log} --all";
+      ll = "${log} --numstat";
+      ls = "${log} --patch";
+
+      # status
+      s = "status --short --branch";
+      st = "status";
+      # stash
+      S = "stash";
+      Sc = "stash clear";
+      Sh = "stash show --patch";
+      Sl = "stash list";
+      Sp = "stash pop";
+      # ls = ''
+      #   log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'';
+      # ll = ''
+      #   log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'';
 
       # aliases for submodule
       update = "submodule update --init --recursive";
