3802e2c67f8dc192f4c2e0861782748baf44fbb2
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-07 22:19:50 +0800

    fix: missing

--- flakes/devshell.nix
+++ flakes/devshell.nix
@@ -7,7 +7,7 @@
     ...
   }: {
     devshells.default = {
-      NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+      # NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
       packages = [config.treefmt.build.wrapper pkgs.nix-diff];
       name = "dots";
       # shellHook = ''
