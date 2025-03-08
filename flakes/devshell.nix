5ef8ac7e9269f59ca19c2a5f436005c2f610b3e4
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-08 21:16:22 +0800

    feat: update lock

--- flakes/devshell.nix
+++ flakes/devshell.nix
@@ -7,12 +7,8 @@
     ...
   }: {
     devshells.default = {
-      # NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
       packages = [config.treefmt.build.wrapper pkgs.nix-diff];
       name = "dots";
-      # shellHook = ''
-      #   ${config.pre-commit.installationScript}
-      # '';
     };
   };
 }
