09508804b9558fe760457e3da2fdc2cc1a3aa3d7
Wenqaing Hu <huwqchn@gmail.com>
2025-03-10 15:14:19 +0800

    feat: don't use cache mirror located in China

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -55,10 +55,10 @@ in {
         "https://nix-community.cachix.org"
         # cache mirror located in China
         # status: https://mirror.sjtu.edu.cn/
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # "https://mirror.sjtu.edu.cn/nix-channels/store"
         # status: https://mirrors.ustc.edu.cn/status/
-        "https://mirrors.ustc.edu.cn/nix-channels/store"
-        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        # "https://mirrors.ustc.edu.cn/nix-channels/store"
+        # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
         # cuda-maintainer's cache server
         "https://cuda-maintainers.cachix.org"
