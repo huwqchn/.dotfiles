39eb97bf7c998145e81a0a5226794093b10f169f
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-10 23:35:45 +0800

    feat: add nix substituters

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -52,6 +52,7 @@ in {
       trusted-users = [config.my.name];
       # substituers that will be considered before the official ones(https://cache.nixos.org)
       substituters = [
+        "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         # cache mirror located in China
         # status: https://mirror.sjtu.edu.cn/
@@ -67,6 +68,7 @@ in {
       # https://github.com/NixOS/nix/issues/9574
       nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
