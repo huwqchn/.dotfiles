1c931caaba3a74042047a83aed06e7c13dacd6f1
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-15 01:42:13 +0800

    feat: add caddy and tailscale for nas

---
+++ modules/nixos/services/caddy.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (config) my;
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.my.caddy;
+in {
+  options.my.caddy = {
+    enable = mkEnableOption "Enable Caddy";
+  };
+
+  config = mkIf cfg.eenable {
+    environment = {
+      shellAliases = {
+        caddy-log = "journalctl _SYSTEMD_UNIT=caddy.service";
+      };
+      systemPackages = with pkgs; [custom-caddy];
+    };
+    services = {
+      caddy = {
+        enable = true;
+        inherit (my) email;
+        globalConfig = ''
+          servers {
+            trusted_proxies cloudflare {
+              interval 12h
+              timeout 15s
+            }
+          }
+        '';
+        package = pkgs.custom-caddy;
+      };
+    };
+  };
+}
---
+++ modules/nixos/services/tailscale.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption;
+  inherit (config) my;
+  cfg = config.my.tailscale;
+  isWorkstation = config.my.machine.type == "workstation";
+in {
+  options.my.tailscale = {
+    enable = mkEnableOption "Enable Tailscale";
+    advertiseExitNode = mkEnableOption "advertise as exit node";
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; lib.optionals isWorkstation [trayscale];
+
+    services.tailscale = {
+      enable = true;
+      extraUpFlags =
+        [
+          "--operator=${my.name}"
+        ]
+        ++ lib.optional cfg.advertiseExitNode "--advertise-exit-node";
+      extraSetFlags =
+        [
+          "--operator=${my.name}"
+        ]
+        ++ lib.optional cfg.advertiseExitNode "--advertise-exit-node";
+      # Enable caddy to acquire certificates from the tailscale daemon
+      # - https://tailscale.com/blog/caddy
+      permitCertUid = lib.mkIf my.caddy.enable "caddy";
+      openFirewall = true;
+      useRoutingFeatures = "both";
+    };
+  };
+}
