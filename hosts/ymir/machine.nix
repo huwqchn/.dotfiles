4fe4ad381445fd0ecdf2b56281d53199e78ed54b
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-15 18:41:56 +0800

    fix: complete

--- hosts/ymir/machine.nix
+++ hosts/ymir/machine.nix
@@ -1,9 +1,17 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.hardware.nixosModules.dell-xps-15-9560-nvidia
   ];
 
   boot.initrd.kernelModules = ["dm-snapshot"];
+  boot.kernelModules = ["kvm-intel"];
+
+  fileSystems."/persist".neededForBoot = true; # required by impermanence
+  fileSystems."/var/log".neededForBoot = true; # required by nixos
 
   boot.supportedFilesystems = [
     "ext4"
@@ -14,6 +22,16 @@
     "vfat"
     "exfat"
   ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   my.machine = {
     type = "laptop";
