15e31459aef23050dfe3e0cc214beb4888a92324
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-15 18:15:21 +0800

    chore: rm comment

--- hosts/ymir/config.nix
+++ hosts/ymir/config.nix
@@ -1,26 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 {
   imports = [
-    # Include the results of the hardware scan.
     ./machine.nix
     ./boot.nix
     ../common/nixosDefault.nix
   ];
-
-  # fix timed out for device /dev/tpmrm0
-  # systemd.units."dev-tpmrm0.device".enable = false;
-
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking = {
-  #   networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  # };
 
   # my custom modules
   my = {
