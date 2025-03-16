5cd73327cecbb6e263ec0701255e7d962a82af12
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-16 15:45:09 +0800

    fix: duplicate option defined

--- home/desktop/wayland/default.nix
+++ home/desktop/wayland/default.nix
@@ -1,17 +1,1 @@
-{
-  lib,
-  config,
-  ...
-}: let
-  inherit (lib) mkEnableOption;
-in {
-  imports = lib.my.scanPaths ./.;
-
-  options.my.desktop.wayland = {
-    enable =
-      mkEnableOption "desktop.wayland"
-      // {
-        default = config.my.desktop.enable;
-      };
-  };
-}
+{lib, ...}: {imports = lib.my.scanPaths ./.;}
