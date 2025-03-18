5177000df60be47e59b820a2b7f1418473f2365a
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-19 04:03:14 +0800

    fix: udev with yubikey only support linux

--- modules/common/yubikey.nix
+++ modules/common/yubikey.nix
@@ -4,8 +4,7 @@
   config,
   ...
 }: let
-  inherit (lib.modules) mkIf;
-  inherit (lib.options) mkOption mkEnableOption;
+  inherit (lib) mkIf mkOption mkEnableOption optionalAttrs;
   cfg = config.my.yubikey;
   homeDirectory = config.my.home;
   yubikey-up = let
@@ -116,46 +115,48 @@ in {
     # We already have a yubikey rule that sets the ENV variable
 
     # FIXME(yubikey): This is linux only
-    services.udev.extraRules = ''
-      # Link/unlink ssh key on yubikey add/remove
-      SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="1050", RUN+="${lib.getBin yubikey-up}/bin/yubikey-up"
-      # NOTE: Yubikey 4 has a ID_VENDOR_ID on remove, but not Yubikey 5 BIO, whereas both have a HID_NAME.
-      # Yubikey 5 HID_NAME uses "YubiKey" whereas Yubikey 4 uses "Yubikey", so matching on "Yubi" works for both
-      SUBSYSTEM=="hid", ACTION=="remove", ENV{HID_NAME}=="Yubico Yubi*", RUN+="${lib.getBin yubikey-down}/bin/yubikey-down"
+    services = optionalAttrs pkgs.stdenv.isLinux {
+      udev.extraRules = ''
+        # Link/unlink ssh key on yubikey add/remove
+        SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="1050", RUN+="${lib.getBin yubikey-up}/bin/yubikey-up"
+        # NOTE: Yubikey 4 has a ID_VENDOR_ID on remove, but not Yubikey 5 BIO, whereas both have a HID_NAME.
+        # Yubikey 5 HID_NAME uses "YubiKey" whereas Yubikey 4 uses "Yubikey", so matching on "Yubi" works for both
+        SUBSYSTEM=="hid", ACTION=="remove", ENV{HID_NAME}=="Yubico Yubi*", RUN+="${lib.getBin yubikey-down}/bin/yubikey-down"
 
-      ##
-      # Yubikey 4
-      ##
+        ##
+        # Yubikey 4
+        ##
 
-      # Lock the device if you remove the yubikey (use udevadm monitor -p to debug)
-      # #ENV{ID_MODEL_ID}=="0407", # This doesn't match all the newer keys
-      # FIXME(yubikey): We only want this to happen if we're undocked, so we need to see how that works. We probably need to run a
-      # script that does smarter checks
-      # ACTION=="remove",\
-      #  ENV{ID_BUS}=="usb",\
-      #  ENV{ID_VENDOR_ID}=="1050",\
-      #  ENV{ID_VENDOR}=="Yubico",\
-      #  RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+        # Lock the device if you remove the yubikey (use udevadm monitor -p to debug)
+        # #ENV{ID_MODEL_ID}=="0407", # This doesn't match all the newer keys
+        # FIXME(yubikey): We only want this to happen if we're undocked, so we need to see how that works. We probably need to run a
+        # script that does smarter checks
+        # ACTION=="remove",\
+        #  ENV{ID_BUS}=="usb",\
+        #  ENV{ID_VENDOR_ID}=="1050",\
+        #  ENV{ID_VENDOR}=="Yubico",\
+        #  RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
 
-      ##
-      # Yubikey 5 BIO
-      #
-      # NOTE: The remove event for the bio doesn't include the ID_VENDOR_ID for some reason, but we can use the
-      # hid name instead. Some HID_NAME might be "Yubico YubiKey OTP+FIDO+CCID" or "Yubico YubiKey FIDO", etc so just
-      # match on "Yubico YubiKey"
-      ##
+        ##
+        # Yubikey 5 BIO
+        #
+        # NOTE: The remove event for the bio doesn't include the ID_VENDOR_ID for some reason, but we can use the
+        # hid name instead. Some HID_NAME might be "Yubico YubiKey OTP+FIDO+CCID" or "Yubico YubiKey FIDO", etc so just
+        # match on "Yubico YubiKey"
+        ##
 
-      # SUBSYSTEM=="hid",\
-      #  ACTION=="remove",\
-      #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
-      #  RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+        # SUBSYSTEM=="hid",\
+        #  ACTION=="remove",\
+        #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
+        #  RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
 
-      # FIXME(yubikey): Change this so it only wakes up the screen to the login screen, xset cmd doesn't work
-      # SUBSYSTEM=="hid",\
-      #  ACTION=="add",\
-      #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
-      #  RUN+="${pkgs.systemd}/bin/loginctl activate 1"
-      #  #RUN+="${lib.getBin pkgs.xorg.xset}/bin/xset dpms force on"
-    '';
+        # FIXME(yubikey): Change this so it only wakes up the screen to the login screen, xset cmd doesn't work
+        # SUBSYSTEM=="hid",\
+        #  ACTION=="add",\
+        #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
+        #  RUN+="${pkgs.systemd}/bin/loginctl activate 1"
+        #  #RUN+="${lib.getBin pkgs.xorg.xset}/bin/xset dpms force on"
+      '';
+    };
   };
 }
