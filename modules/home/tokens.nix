0ce9139bfd9938cff9b07636b00fd1fad2d6ff70
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-20 06:41:00 +0800

    feat: add some ai plugin api key to agenix secrets

---
+++ modules/home/tokens.nix
@@ -0,0 +1,50 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  user_readable = {
+    symlink = false;
+    owner = config.my.name;
+    mode = "0500";
+  };
+in {
+  config = lib.mkMerge [
+    (
+      lib.mkIf config.hm.my.neovim.lazyvim.copilot.enable {
+        age.secrets = {
+          github-copilot =
+            {
+              rekeyFile = ./github-copilot.age;
+              path = "${config.my.home}/.config/github-copilot/apps.json";
+            }
+            // user_readable;
+        };
+      }
+    )
+    (
+      lib.mkIf config.hm.my.neovim.lazyvim.supermaven.enable {
+        age.secrets = {
+          supermaven =
+            {
+              rekeyFile = ./supermaven.age;
+              path = "${config.my.home}/.supermaven/config.json";
+            }
+            // user_readable;
+        };
+      }
+    )
+    (
+      lib.mkIf config.hm.my.neovim.lazyvim.codeium.enable {
+        age.secrets = {
+          codeium =
+            {
+              rekeyFile = ./codeium.age;
+              path = "${config.my.home}/.cache/nvim/codeium/config.json";
+            }
+            // user_readable;
+        };
+      }
+    )
+  ];
+}
