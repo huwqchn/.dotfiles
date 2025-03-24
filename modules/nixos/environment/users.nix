a35eb369ffd57f7eb1646c1fd3717c4836bbf37e
Wenqaing Hu <johnson.wq.hu@gmail.com>
2025-03-24 10:45:18 +0800

    feat: rearrange some config

--- modules/nixos/environment/users.nix
+++ modules/nixos/environment/users.nix
@@ -1,10 +1,13 @@
-{config, ...}: let
+{config, pkgs, ...}: let
   cfgUser = config.users.users."${config.my.name}";
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
   users = {
     # Don't allow mutation of users outside the config.
     mutableUsers = false;
+
+    # set user's default shell system-wide
+    defaultUserShell = pkgs.bashInteractive;
 
     groups = {
       "${config.my.name}" = {};
@@ -36,6 +39,7 @@ in {
             "adbusers"
             "libvirtd"
           ];
+        shell = pkgs.fish;
       };
       # root's ssh key are mainly used for remote deployment
       root = {
