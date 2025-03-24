23edd93866b28fea8252f732d4455ece47ca483e
Wenqaing Hu <johnson.wq.hu@gmail.com>
2025-03-24 10:10:48 +0800

    feat: add jellyfin service

---
+++ modules/nixos/services/selfhosted/jellyfin.nix
@@ -0,0 +1,24 @@
+{ lib, config, ... }: let
+  cfg = confgi.my.services.jellyfin;
+  inherit (lib) mkIf mkEnableOption;
+in {
+  options.my.services.jellyfin = {
+    enable = mkEnableOption "jellyfin";
+    port = lib.mkOption {
+      type = lib.types.int;
+      default = 8096;
+      description = "The port on which Jellyfin will listen.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.jellyfin.enable = true;
+
+    networking.firewall = {
+      allowedTCPPorts = [ cfg.port ];
+      allowedUDPPorts = [ cfg.port ];
+    };
+
+    user.extraGroups = [ "jellyfin" ];
+  };
+}
