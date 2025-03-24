5c82d842b9e2f60f15e6c88c53854156f3b50581
Wenqaing Hu <johnson.wq.hu@gmail.com>
2025-03-24 10:29:49 +0800

    feat: more hardware -- hidpi printer

---
+++ modules/nixos/hardware/printer.nix
@@ -0,0 +1,16 @@
+{ lib, config, pkgs, ... }: let
+  inherit (lib) mkEnableOption mkIf;
+  inherit (config.my.machine) hasPrinter;
+
+in {
+  options.my.machine.hasPrinter = mkEnableOption "Whether has printer support";
+
+  config = mkIf hasPrinter {
+    services.printing = {
+      enable = true;
+      startWhenNeeded = true;
+      # logLevel = "debug";
+    };
+  };
+
+}
