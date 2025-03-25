3729eee70bb22f3300a101bce8e9ec05ac881921
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-25 23:54:43 +0800

    feat: add desktop programs

---
+++ lib/modules.nix
@@ -0,0 +1,13 @@
+{lib}: let
+  inherit (lib.options) mkEnableOption mkPackageOption;
+  inherit (lib.attrsets) recursiveUpdate;
+
+  mkProgram = pkgs: name: extraConfig:
+    recursiveUpdate {
+      enable = mkEnableOption "Enable ${name}";
+      package = mkPackageOption pkgs name {};
+    }
+    extraConfig;
+in {
+  inherit mkProgram;
+}
---
+++ modules/nixos/desktop/display/wayland/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib.meta) getExe;
+  inherit (lib.modules) mkIf mkDefault;
+  inherit (lib.strings) optionalString;
+
+  inherit (lib.my) shell;
+  isHyprland = config.my.desktop.default == "hyprland";
+  cfg = config.my.desktop.wayland;
+in {
+  config = mkIf cfg.enable {
+    services.xserver.enable = mkDefault false;
+
+    environment = {
+      etc."greetd/environments".text = mkIf config.services.greetd.enable ''
+        ${optionalString isHyprland "Hyprland"}
+        ${shell}
+      '';
+
+      variables = {
+        NIXOS_OZONE_WL = "1";
+        _JAVA_AWT_WM_NONEREPARENTING = "1";
+        GDK_BACKEND = "wayland,x11";
+        ANKI_WAYLAND = "1";
+        MOZ_ENABLE_WAYLAND = "1";
+        XDG_SESSION_TYPE = "wayland";
+        SDL_VIDEODRIVER = "wayland";
+        CLUTTER_BACKEND = "wayland";
+        #WLR_DRM_NO_ATOMIC = "1";
+        #WLR_BACKEND = "vulkan";
+        #__GL_GSYNC_ALLOWED = "0";
+        #__GL_VRR_ALLOWED = "0";
+      };
+    };
+
+    systemd.services.seatd = {
+      enable = true;
+      description = "Seat management daemon";
+      script = "${getExe pkgs.seatd} -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "1";
+      };
+      wantedBy = ["multi-user.target"];
+    };
+  };
+}
