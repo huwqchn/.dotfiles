0f02f993f6085a9e96284d0820b748024e073445
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-26 11:08:56 +0800

    fix: consume pkgs by withSystem

--- flakes/hosts.nix
+++ flakes/hosts.nix
@@ -2,7 +2,8 @@
   self,
   inputs,
   lib,
+  withSystem,
   ...
 }: let
-  specialArgs = {inherit self inputs lib;};
+  specialArgs = {inherit self inputs lib withSystem;};
 in {flake = lib.my.mkHosts ../hosts specialArgs;}
