c3d83732826dedb4826d000a594ecde66e412839
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-26 20:50:46 +0800

    fix: tweak fish

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -4,8 +4,9 @@
   lib,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkEnableOption mkIf optionalAttrs;
   cfg = config.my.fish;
+  inherit (pkgs.stdenv) isLinux;
 in {
   options.my.fish = {
     enable = mkEnableOption "fish" // {default = config.my.shell == "fish";};
@@ -43,10 +44,10 @@ in {
           name = "colored-man-pages";
           inherit (pkgs.fishPlugins.colored-man-pages) src;
         }
-        {
-          name = "fish-you-should-use";
-          inherit (pkgs.fishPlugins.fish-you-should-use) src;
-        }
+        # {
+        #   name = "fish-you-should-use";
+        #   inherit (pkgs.fishPlugins.fish-you-should-use) src;
+        # }
         {
           name = "bang-bang";
           inherit (pkgs.fishPlugins.bang-bang) src;
@@ -67,76 +68,79 @@ in {
             exec tmux attach || tmux new
         end
       '';
-      shellAbbrs = {
-        c = "clear";
-        Q = "shutdown -h now";
-        R = "reboot";
-        mv = "mv -iv";
-        cp = "cp -riv";
-        mkdir = "mkdir -vp";
-        rmdir = "rmdir -vp";
-        v = "nvim";
-        lg = "lazygit";
-        lzd = "lazydocker";
-        ipy = "ipython";
-        cc = "cc -Wall -Werror -Wextra";
-        pc = "proxychains4";
-        icat = "wezterm imgcat";
-        ns = "netstat -tunlp";
-        # cd
-        ".." = "cd ..";
-        "..." = "cd ../..";
-        ".3" = "cd ../../..";
-        ".4" = "cd ../../../..";
-        ".5" = "cd ../../../../..";
-        # git
-        g = "git";
-        gi = "git clone";
-        gl = "git l --color | devmoji --log --color | less -rXF";
-        gs = "git st";
-        gb = "git checkout -b";
-        gc = "git commit";
-        gca = "git commit --amend -a --no-edit";
-        gpr = "git pr checkout";
-        gm = "git branch -l main | rg main > /dev/null 2>&1 && git checkout main || git checkout master";
-        gcp = "git commit -p";
-        gP = "git push";
-        gp = "git pull";
-        ggc = "git reflog expire --expire-unreachable=now --all; and git gc --prune=now";
-        # tmux
-        t = "tmux";
-        tc = "tmux attach";
-        ta = "tmux attact -t";
-        tad = "tmux attact -d -t";
-        ts = "tmux new -s";
-        tl = "tmux ls";
-        tk = "tmux kill-session -t";
-        # trashy
-        rp = "trash put";
-        rl = "trash list";
-        rr = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trash restore --match=exact --force";
-        re = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trash empty --match=exact --force";
-        # systemctl
-        s = "systemctl";
-        su = "systemctl --user";
-        ss = "systemctl status";
-        sl = "systemctl --type service --state running";
-        slu = "systemctl --user --type service --state running";
-        se = "sudo systemctl enable";
-        sd = "sudo systemctl disable";
-        sen = "sudo systemctl enable --now";
-        sdn = "sudo systemctl disable --now";
-        sr = "sudo systemctl restart";
-        so = "sudo systemctl stop";
-        sa = "sudo systemctl start";
-        sf = "systemctl --failed --all";
-        # journalctl
-        j = "journalctl";
-        jb = "journalctl -b";
-        jf = "journalctl --follow";
-        jg = "journalctl -b --grep";
-        ju = "journalctl --unit";
-      };
+      shellAbbrs =
+        {
+          c = "clear";
+          Q = "shutdown -h now";
+          R = "reboot";
+          mv = "mv -iv";
+          cp = "cp -riv";
+          mkdir = "mkdir -vp";
+          rmdir = "rmdir -vp";
+          v = "nvim";
+          lg = "lazygit";
+          lzd = "lazydocker";
+          ipy = "ipython";
+          cc = "cc -Wall -Werror -Wextra";
+          pc = "proxychains4";
+          icat = "wezterm imgcat";
+          ns = "netstat -tunlp";
+          # cd
+          ".." = "cd ..";
+          "..." = "cd ../..";
+          ".3" = "cd ../../..";
+          ".4" = "cd ../../../..";
+          ".5" = "cd ../../../../..";
+          # git
+          g = "git";
+          gi = "git clone";
+          gl = "git l --color | devmoji --log --color | less -rXF";
+          gs = "git st";
+          gb = "git checkout -b";
+          gc = "git commit";
+          gca = "git commit --amend -a --no-edit";
+          gpr = "git pr checkout";
+          gm = "git branch -l main | rg main > /dev/null 2>&1 && git checkout main || git checkout master";
+          gcp = "git commit -p";
+          gP = "git push";
+          gp = "git pull";
+          ggc = "git reflog expire --expire-unreachable=now --all; and git gc --prune=now";
+          # tmux
+          t = "tmux";
+          tc = "tmux attach";
+          ta = "tmux attact -t";
+          tad = "tmux attact -d -t";
+          ts = "tmux new -s";
+          tl = "tmux ls";
+          tk = "tmux kill-session -t";
+        }
+        // optionalAttrs isLinux {
+          # trashy
+          rp = "trash put";
+          rl = "trash list";
+          rr = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trash restore --match=exact --force";
+          re = "trash list | fzf --multi | awk '{$1=$1;print}' | rev | cut -d ' ' -f1 | rev | xargs trash empty --match=exact --force";
+          # systemctl
+          s = "systemctl";
+          su = "systemctl --user";
+          ss = "systemctl status";
+          sl = "systemctl --type service --state running";
+          slu = "systemctl --user --type service --state running";
+          se = "sudo systemctl enable";
+          sd = "sudo systemctl disable";
+          sen = "sudo systemctl enable --now";
+          sdn = "sudo systemctl disable --now";
+          sr = "sudo systemctl restart";
+          so = "sudo systemctl stop";
+          sa = "sudo systemctl start";
+          sf = "systemctl --failed --all";
+          # journalctl
+          j = "journalctl";
+          jb = "journalctl -b";
+          jf = "journalctl --follow";
+          jg = "journalctl -b --grep";
+          ju = "journalctl --unit";
+        };
       functions = {
         extract = {
           body = ''
