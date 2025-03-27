14923207bdbb044175e66fead134373e283348c5
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-28 01:16:45 +0800

    feat: readd wine

---
+++ modules/nixos/desktop/programs/wine.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (lib.lists) optional;
+  isWayland = config.my.desktop.wayland.enable;
+  cfg = config.my.programs.wine;
+in {
+  options.my.programs.wine = lib.my.mkProgram pkgs "wine" {
+    package.default =
+      if isWayland config
+      then pkgs.wineWowPackages.waylandFull
+      else pkgs.wineWowPackages.stableFull;
+  };
+
+  # determine which version of wine to use
+  config.environment.systemPackages = optional cfg.enable cfg.package;
+}
