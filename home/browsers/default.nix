2b0d5fe718bacfea5ccb2892c4998906e28ed96d
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-29 20:14:41 +0800

    feat: rm unmanaged browser config

--- home/browsers/default.nix
+++ home/browsers/default.nix
@@ -1,12 +1,3 @@
-{lib, ...}: let
-  inherit (lib) mkOption types;
-  # browser = config.my.browser;
-in {
+{lib, ...}: {
   imports = lib.my.scanPaths ./.;
-
-  options.my.browser = mkOption {
-    type = types.enum ["firefox" "chrom" "zen"];
-    default = "zen";
-    description = "The browser to use";
-  };
 }
