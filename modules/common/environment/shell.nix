d59fe7b4a25157c895500594cdffa87cca56c4e5
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-29 23:27:47 +0800

    feat: enable both zsh and fish

--- modules/common/environment/shell.nix
+++
@@ -1,9 +0,0 @@
-{config, ...}: let
-  inherit (config.my) shell;
-in {
-  programs = {
-    fish.enable = shell == "fish";
-    zsh.enable = shell == "zsh";
-    # nushell.enable = shell == "nushell";
-  };
-}
