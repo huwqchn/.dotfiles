94e30c1b606915fb9757d34671ebccb7239002c5
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-29 22:57:03 +0800

    refactor: nix settings

---
+++ lib/hardware.nix
@@ -0,0 +1,78 @@
+_: let
+  /*
+  *
+  check if the host platform is linux and x86
+
+  # Arguments
+
+  - [pkgs] the package set
+
+  # Type
+
+  ```
+  isx86Linux :: AttrSet -> Bool
+  ```
+
+  # Example
+
+  ```nix
+  isx86Linux pkgs
+  => true
+  ```
+  */
+  isx86Linux = pkgs: pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isx86;
+
+  /*
+  *
+  ldTernary, short for linux darwin ternary
+
+  # Inputs
+
+  - [pkgs] is the package set
+  - [l] the value to return if the host platform is linux
+  - [d] the value to return if the host platform is darwin
+
+  # Type
+
+  ```
+  ldTernary :: AttrSet -> Any -> Any -> Any
+  ```
+
+  # Example
+
+  ```nix
+  ldTernary pkgs "linux" "darwin"
+  => "linux"
+  ```
+  */
+  ldTernary = pkgs: l: d:
+    if pkgs.stdenv.hostPlatform.isLinux
+    then l
+    else if pkgs.stdenv.hostPlatform.isDarwin
+    then d
+    else throw "Unsupported system: ${pkgs.stdenv.system}";
+  /*
+  *
+  assume the first monitor in the list of monitors is primary
+  get its name from the list of monitors
+
+  # Arguments
+
+  - [config] the configuration that nixosConfigurations provides
+
+  # Type
+
+  ```
+  primaryMonitor :: AttrSet -> String
+  ```
+
+  # Example
+
+  ```nix
+  primaryMonitor osConfig
+  => "DP-1"
+  ```
+  */
+in {
+  inherit isx86Linux ldTernary;
+}
--- modules/common/nix/environment.nix
+++ modules/common/nix/environment.nix
@@ -1,12 +1,48 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (builtins) elem;
+  inherit (lib.trivial) pipe;
+  inherit (lib.lists) optionals;
+  inherit (lib.attrsets) filterAttrs mapAttrs';
+  inherit (lib.modules) mkForce;
+in {
   # for security reasons, do not load neovim's user config
   # since EDITOR may be used to edit some critical files
   environment = {
-    variables.EDITOR = "nvim --clean";
+    variables = {
+      EDITOR = "nvim --clean";
+      NIXPKGS_CONFIG = mkForce "";
+    };
 
-    etc."nixos/configuration.nix".source = pkgs.writeText "configuration.nix" ''
-      assert builtins.trace "This is a dummy config, please deploy via the flake!" false;
-      { }
-    '';
+    # etc."nixos/configuration.nix".source = pkgs.writeText "configuration.nix" ''
+    #   assert builtins.trace "This is a dummy config, please deploy via the flake!" false;
+    #   { }
+    # '';
+    # something something backwards compatibility something something nix channels
+    etc = let
+      inherit (config.nix) registry;
+      commonPaths =
+        [
+          "nixpkgs"
+          "tgirlpkgs"
+          "home-manager"
+        ]
+        ++ optionals pkgs.stdenv.hostPlatform.isDarwin [
+          "nix-darwin"
+        ];
+    in
+      pipe registry [
+        (filterAttrs (name: _: (elem name commonPaths)))
+        (mapAttrs' (
+          name: value: {
+            name = "nix/path/${name}";
+            value.source = value.flake;
+          }
+        ))
+      ];
   };
 }
