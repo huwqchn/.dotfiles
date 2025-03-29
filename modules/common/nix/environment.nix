e09b633e3d6498f36b26dec63cda723d7ea46223
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-29 21:03:31 +0800

    feat: change nix stateVersion to 25.05, may fixed some issue

---
+++ modules/common/nix/environment.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}: {
+  # for security reasons, do not load neovim's user config
+  # since EDITOR may be used to edit some critical files
+  environment = {
+    variables.EDITOR = "nvim --clean";
+
+    etc."nixos/configuration.nix".source = pkgs.writeText "configuration.nix" ''
+      assert builtins.trace "This is a dummy config, please deploy via the flake!" false;
+      { }
+    '';
+  };
+}
