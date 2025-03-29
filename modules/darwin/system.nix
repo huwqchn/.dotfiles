4dcfd2461d08f8e778ffa4533a609890cbc8a860
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-29 16:22:53 +0800

    feat: rm top-right corner to lock screen on macos.

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -17,7 +17,7 @@
 
         # customize Hot Corners(触发角, 鼠标移动到屏幕角落时触发的动作)
         wvous-tl-corner = 2; # top-left - Mission Control
-        wvous-tr-corner = 13; # top-right - Lock Screen
+        # wvous-tr-corner = 13; # top-right - Lock Screen
         wvous-bl-corner = 3; # bottom-left - Application Windows
         wvous-br-corner = 4; # bottom-right - Desktop
       };
