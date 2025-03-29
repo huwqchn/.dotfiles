5e72c0a4b4c3720ef802f208eabb9877b18db3ec
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-30 01:02:49 +0800

    feat: completed ymir config

--- hosts/ymir/config.nix
+++ hosts/ymir/config.nix
@@ -1,17 +1,70 @@
 {
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
-    ./machine.nix
+    inputs.hardware.nixosModules.dell-xps-15-9560-nvidia
+    ../common/disko/luks-btrfs-tmpfs.nix
   ];
 
-  # my custom modules
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
   my = {
-    onedrive.enable = true;
-    fhs.enable = true;
+    boot = {
+      secureBoot = false;
+      tmpOnTmpfs = true;
+      enableKernelTweaks = true;
+      loadRecommendedModules = true;
+      plymouth.enable = true;
+
+      initrd = {
+        enableTweaks = true;
+        optimizeCompressor = true;
+      };
+
+      fs = [
+        "ext4"
+        "btrfs"
+        "xfs"
+        "ntfs"
+        "fat"
+        "vfat"
+        "exfat"
+      ];
+    };
+    yubikey.enable = true;
+    video.enable = true;
     btrbk.enable = true;
     zram.enable = true;
-    power.enable = true;
-    virtual.enable = true;
+    game.enable = false;
     security.enable = true;
-    desktop.enable = true;
+    services = {
+      onedrive.enable = true;
+      printer.enable = true;
+    };
+    desktop = {
+      enable = true;
+      wayland.enable = true;
+      default = "hyprland";
+    };
+    virtual.enable = true;
+    machine = {
+      type = "laptop";
+      cpu = "intel";
+      gpu = "nvidia";
+      hasSound = true;
+      hasBluetooth = true;
+      hasPrinter = false;
+      hasTPM = false;
+      isHidpi = true;
+      persist = true;
+    };
   };
 }
