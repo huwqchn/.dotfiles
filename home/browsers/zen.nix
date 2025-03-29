1c0ac69a6c7507954ca7690e4ee9e03665d07cbd
Johnson Hu <johnson.wq.hu@gmail.com>
2025-03-29 20:15:41 +0800

    feat: enable browser when apps enable

--- home/browsers/zen.nix
+++ home/browsers/zen.nix
@@ -9,7 +9,11 @@
   cfg = config.my.desktop.apps.browser;
 in {
   options.my.desktop.apps.browser = {
-    enable = mkEnableOption "zen";
+    enable =
+      mkEnableOption "zen"
+      // {
+        default = config.my.desktop.apps.enable;
+      };
   };
   config = mkIf cfg.enable {
     home.packages = [inputs.zen.packages.${pkgs.system}.default];
