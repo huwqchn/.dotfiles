a21e3fa8af67b0edc4db6c8dddacc8fac251741b
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-09 05:21:42 +0800

    feat: updat yazi git icon

--- home/cli/yazi.nix
+++ home/cli/yazi.nix
@@ -50,17 +50,26 @@ in {
       initLua = ''
         require("starship"):setup()
 
+        -- https://github.com/yazi-rs/plugins/tree/main/git.yazi
         th.git = th.git or {}
-        th.git.modified = ui.Style():fg("blue")
-        th.git.deleted = ui.Style():fg("red")
-        th.git.added = ui.Style():fg("green")
-        th.git.ignored = ui.Style():fg("darkgray")
 
-        th.git.modified_sign = "M"
-        th.git.added_sign = "A"
-        th.git.untracked_sign = "U"
-        th.git.ignored_sign = "I"
-        th.git.deleted_sign = "D"
+        th.git.added = ui.Style():fg("blue")
+        th.git.added_sign = ""
+
+        th.git.deleted = ui.Style():fg("red"):bold()
+        th.git.deleted_sign = ""
+
+        th.git.ignored = ui.Style():fg("gray")
+        th.git.ignored_sign = ""
+
+        th.git.modified = ui.Style():fg("green")
+        th.git.modified_sign = ""
+
+        th.git.untracked = ui.Style():fg("gray")
+        th.git.untracked_sign = ""
+
+        th.git.updated = ui.Style():fg("green")
+        th.git.updated_sign = ""
 
         require("git"):setup()
       '';
