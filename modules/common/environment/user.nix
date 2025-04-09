f14729326210e22a74cdda4a184aecbdab2c7f03
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-09 19:20:57 +0800

    feat: create ~/.ssh for agenix home module

--- modules/common/environment/user.nix
+++ modules/common/environment/user.nix
@@ -41,10 +41,6 @@ in {
     description = name;
   };
 
-  systemd.tmpfiles.rules = [
-    "d ${home}/.ssh 0700 ${name} ${userGroup} -"
-  ];
-
   age.secrets.my-ssh-key =
     {
       rekeyFile = "${self}/secrets/${name}/ssh.age";
