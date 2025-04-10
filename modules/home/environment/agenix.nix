a730d6baeb2a4f2e7d2cc43adb6466dabdfdeb73
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-10 12:05:31 +0800

    fix: agenix homeManager running error

--- modules/home/environment/agenix.nix
+++ modules/home/environment/agenix.nix
@@ -1,9 +1,12 @@
 {
   inputs,
   config,
+  lib,
+  pkgs,
   ...
 }: let
   inherit (config.home) homeDirectory;
+  isLinuxSystemd = pkgs.stdenv.isLinux && config.systemd.user.services ? agenix;
 in {
   imports = [
     inputs.agenix.homeManagerModules.default
@@ -15,4 +18,12 @@ in {
     secretsDir = "${homeDirectory}/.agenix";
     secretsMountPoint = "${homeDirectory}/.agenix/agenix.d";
   };
+
+  home.activation.agenix = lib.hm.dag.entryAfter ["writeBoundary"] (
+    if isLinuxSystemd
+    then ''
+      ${config.systemd.user.services.agenix.Service.ExecStart}
+    ''
+    else ""
+  );
 }
