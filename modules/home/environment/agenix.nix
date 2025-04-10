f53c209ea1084f2f5c03929e23fd2abb3cf682e7
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-11 00:50:42 +0800

    chore: how to use agenix home-manager module on nixos

--- modules/home/environment/agenix.nix
+++ modules/home/environment/agenix.nix
@@ -24,6 +24,8 @@ in {
     rage
   ];
 
+  # You must set this, if you want to use agenix home manager module on nixos
+  # see: https://github.com/ryantm/agenix/issues/50#issuecomment-1926893522
   home.activation.agenix = lib.hm.dag.entryAfter ["writeBoundary"] (
     if isLinuxSystemd
     then ''
