4b57681436ef612a04a521de234c158edc224ae0
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-11 12:32:54 +0800

    feat: mv linux desktop theme to my themes modules

--- modules/home/themes/tokyonight/default.nix
+++ modules/home/themes/tokyonight/default.nix
@@ -6,7 +6,12 @@
   ...
 }: let
   src = pkgs.vimPlugins.tokyonight-nvim;
-  inherit (lib) mkIf mkEnableOption mkOption types mkMerge;
+  inherit (lib.options) mkEnableOption mkOption;
+  inherit (lib.modules) mkForce mkIf mkMerge importTOML;
+  inherit (lib.types) enum;
+  inherit (lib.generators) toINIWithGlobalSection;
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
+
   cfg = config.my.tokyonight;
   themeName = "tokyonight_${cfg.style}";
 in {
@@ -18,7 +23,7 @@ in {
       };
 
     style = mkOption {
-      type = types.enum ["night" "storm" "day" "moon"];
+      type = enum ["night" "storm" "day" "moon"];
       default = "moon";
       description = "The style of tokyonight";
     };
@@ -29,6 +34,7 @@ in {
   config = mkMerge [
     (mkIf cfg.enable {
       home.sessionVariables = {THEME = themeName;};
+      services.dunst.settings = importTOML "${src}/extras/dunst/tokyonight_${cfg.style}.dunstrc";
       programs = {
         git = {
           includes = [{path = "${src}/extras/delta/${themeName}.gitconfig";}];
@@ -201,6 +207,105 @@ in {
 
         return M
       '';
+    })
+    (mkIf (cfg.enable && isLinux) {
+      i18n.inputMethod.fcitx5.addons = with pkgs; [fcitx5-tokyonight];
+      xdg.configFile."fcitx5/conf/classicui.conf" = {
+        enable = config.i18n.inputMethod.enabled == "fcitx5";
+        text = let
+          shade =
+            if cfg.style == "day"
+            then "Day"
+            else "Storm";
+        in
+          toINIWithGlobalSection {} {
+            globalSection = {
+              Theme = "Tokyonight-${shade}";
+              DarkTHeme = "Tokyonight-${shade}";
+            };
+          };
+      };
+
+      # FIXME: please tweak this to tokyonight theme
+
+      # If your themes for mouse cursor, icons or windows don't load correctly,
+      # try setting them with home.pointerCursor and gtk.theme,
+      # which enable a bunch of compatibility options that should make the themes load in all situations.
+
+      home.pointerCursor = {
+        gtk.enable = true;
+        x11.enable = true;
+        package = lib.mkForce pkgs.bibata-cursors;
+        name = lib.mkForce "Bibata-Modern-Ice";
+        size = lib.mkForce 24;
+      };
+      dconf.settings."org/gnome/desktop/interface".font-name =
+        lib.mkForce "Cantarell";
+
+      # set dpi for 4k monitor
+      xresources.properties = {
+        # dpi for Xorg.s font
+        "Xft.dpi" = 192;
+        # or set a generic dpi
+        "*.dpi" = 192;
+
+        # These might also be useful depending on your monitor and personal preferences.
+        "Xft.autohint" = 0;
+        "Xft.lcdfilter" = "lcddefault";
+        "Xft.hintstyle" = "hintfull";
+        "Xft.antialias" = 1;
+        "Xft.rgba" = "rgb";
+      };
+
+      # gkt's theme settings, generate files:
+      #   1. ~/.gtkrc-2.0
+      #   2. ~/.config/gtk-3.0/settings.ini
+      #   3. ~/.config/gtk-4.0/settings.ini
+      gtk = {
+        enable = true;
+
+        gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+        font = {
+          name = mkForce "Cantarell";
+          package = mkForce pkgs.cantarell-fonts;
+          size = mkForce 11;
+        };
+
+        iconTheme = {
+          name = "Papirus-Dark";
+          package = pkgs.papirus-icon-theme;
+        };
+
+        theme = {
+          name = mkForce "Tokyonight-Dark-BL";
+          package = mkForce pkgs.tokyonight-gtk-theme;
+        };
+
+        # cursorTheme = {
+        #   name = "Bibata-Modern";
+        #   package = pkgs.bibata-cursors;
+        #   size = 0;
+        # };
+      };
+
+      qt = {
+        enable = true;
+        platformTheme.name = "qtct";
+        style = {
+          package = pkgs.catppuccin-kvantum;
+          name = "Kvantum";
+        };
+      };
+
+      # xdg.configFile = {
+      #   "Kvantum/kvantum.kvconfig".text = ''
+      #     [General]
+      #     theme=GraphiteNordDark
+      #   '';
+      #
+      #   "Kvantum/GraphiteNord".source = "${pkgs.graphite-kde-theme}/share/Kvantum/GraphiteNord";
+      # };
     })
   ];
 }
