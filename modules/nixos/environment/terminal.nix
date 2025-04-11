92120303af002f028b4ad6f50a9c394d46e49cd3
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-11 17:05:02 +0800

    feat: ssh config

---
+++ modules/nixos/environment/terminal.nix
@@ -0,0 +1,5 @@
+{
+  # Add terminfo database of all known terminals to the system profile.
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/config/terminfo.nix
+  environment.enableAllTerminfo = true;
+}
--- modules/nixos/networking/ssh.nix
+++ modules/nixos/networking/ssh.nix
@@ -8,10 +8,41 @@
 in {
   services.openssh = {
     enable = true;
+
     settings = {
+      # allow root login to remote deployments
       PermitRootLogin = "yes";
       PasswordAuthentication = true;
+
+      # It's default to true
+      # allowSFTP = true;
+
+      # Use key exchange algorithms recommended by `nixpkgs#ssh-audit`
+      KexAlgorithms = [
+        "curve25519-sha256"
+        "curve25519-sha256@libssh.org"
+        "diffie-hellman-group16-sha512"
+        "diffie-hellman-group18-sha512"
+        "sntrup761x25519-sha512@openssh.com"
+        "diffie-hellman-group-exchange-sha256"
+        "mlkem768x25519-sha256"
+        "sntrup761x25519-sha512"
+      ];
+
+      # Use Macs recommended by `nixpkgs#ssh-audit`
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+      ];
+
+      # kick out inactive sessions
+      ClientAliveCountMax = 5;
+      ClientAliveInterval = 60;
     };
+    openFirewall = true;
+    # the port(s) openssh daemon should listen on
+    ports = [22];
     hostKeys = [
       {
         path = "${optionalString persist "/persist"}/etc/ssh/ssh_host_ed25519_key";
@@ -22,13 +53,5 @@ in {
         type = "rsa";
       }
     ];
-    openFirewall = true;
   };
-
-  # networking.firewall.allowedTCPPorts = [ 22 ];
-  networking.firewall.enable = false;
-
-  # Add terminfo database of all known terminals to the system profile.
-  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/config/terminfo.nix
-  environment.enableAllTerminfo = true;
 }
