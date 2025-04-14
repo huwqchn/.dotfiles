22fbdfa86771ee6e2aee49e3b9e83f9cd16fb6c3
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-14 12:43:25 +0800

    feat: use stylix by default for all my hardcoded colors and fonts and cursor

--- modules/home/themes/auto/default.nix
+++ modules/home/themes/auto/default.nix
@@ -1,16 +1,12 @@
 {
   lib,
   config,
-  pkgs,
-  inputs,
   ...
 }: let
   inherit (lib.options) mkOption mkEnableOption;
-  inherit (lib.modules) mkIf mkMerge;
+  inherit (lib.modules) mkIf mkForce;
   inherit (lib.types) enum;
-  inherit (pkgs.stdenv) isLinux;
   cfg = config.my.themes.auto;
-  wall = config.my.themes.wallpaper;
 in {
   # this theme is based on your wallpaper
   options.my.themes.auto = {
@@ -37,46 +33,11 @@ in {
     };
   };
 
-  imports = [
-    inputs.stylix.homeManagerModules.stylix
-  ];
-
-  config = mkMerge [
-    (mkIf cfg.enable {
-      stylix = {
-        enable = true;
-        autoEnable = true;
-        image = wall;
-        polarity = cfg.style;
-      };
-
-      home.file.".config/gowall/config.yml".text = with config.lib.stylix.colors; ''
-        themes:
-          - name: "global"
-            colors:
-              - "#${base00}"
-              - "#${base01}"
-              - "#${base02}"
-              - "#${base03}"
-              - "#${base04}"
-              - "#${base05}"
-              - "#${base06}"
-              - "#${base07}"
-              - "#${base08}"
-              - "#${base09}"
-              - "#${base0A}"
-              - "#${base0B}"
-              - "#${base0C}"
-              - "#${base0D}"
-              - "#${base0E}"
-              - "#${base0F}"
-      '';
-    })
-    (mkIf (cfg.enable && isLinux) {
-      stylix.targets = {
-        hyprpaper.enable = true;
-      };
-      # TODO:
-    })
-  ];
+  config = mkIf cfg.enable {
+    stylix = {
+      autoEnable = true;
+      polarity = cfg.style;
+    };
+    home.file.".config/gowall/config.ymal".text = mkForce "";
+  };
 }
--- modules/home/themes/default.nix
+++ modules/home/themes/default.nix
@@ -1,11 +1,54 @@
 {
+  inputs,
   lib,
   pkgs,
+  config,
   ...
-}: {
-  imports = lib.my.scanPaths ./.;
-  # use gowall to generate colorscheme
+}: let
+  inherit (lib.my) scanPaths;
+  inherit (lib.modules) mkDefault;
+  wall = config.my.themes.wallpaper;
+in {
+  imports =
+    (scanPaths ./.)
+    ++ [
+      inputs.stylix.homeManagerModules.stylix
+    ];
+  stylix = {
+    enable = true;
+    autoEnable = mkDefault false;
+    # TODO: TEST this, since I change a lot of things
+    image = wall;
+    # TODO: use stylix manage my font and cursor
+  };
+
+  # TODO: use gowall to change wallpaper to apply my colorscheme
   home.packages = with pkgs; [
     gowall
   ];
+
+  # WARNING: This should not be used in `auto` theme, cause gowall is change a wallpaper to specific colorscheme
+  # but `auto` theme is generate a colorshecme based on wallpaper
+  # TODO: should make gowall working seemlessly
+  home.file.".config/gowall/config.yml".text = with config.lib.stylix.colors; ''
+    themes:
+      - name: "global"
+        colors:
+          - "#${base00}"
+          - "#${base01}"
+          - "#${base02}"
+          - "#${base03}"
+          - "#${base04}"
+          - "#${base05}"
+          - "#${base06}"
+          - "#${base07}"
+          - "#${base08}"
+          - "#${base09}"
+          - "#${base0A}"
+          - "#${base0B}"
+          - "#${base0C}"
+          - "#${base0D}"
+          - "#${base0E}"
+          - "#${base0F}"
+  '';
 }
