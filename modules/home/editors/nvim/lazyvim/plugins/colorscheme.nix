5ec809a765be9d257ac519a460620fb4b0942182
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-17 18:04:15 +0800

    feat: make lazyvim respect my theme --skip-check

--- modules/home/editors/nvim/lazyvim/plugins/colorscheme.nix
+++ modules/home/editors/nvim/lazyvim/plugins/colorscheme.nix
@@ -4,29 +4,39 @@
   pkgs,
   ...
 }: let
-  inherit (lib.options) mkOption;
-  inherit (lib.types) enum;
+  # inherit (lib.options) mkOption;
+  # inherit (lib.types) enum;
   inherit (lib.modules) mkMerge;
   inherit (lib.my) sourceLua;
-  cfg = config.my.neovim.lazyvim.colorscheme;
+  inherit (config.my.themes) theme;
 in {
-  # FIXME: This should be controlled by `my.thesmes` (my rice system)
-  options.my.neovim.lazyvim.colorscheme = mkOption {
-    type = enum ["tokyonight" "kanagawa" "catppuccin"];
-    default = "tokyonight";
-    description = ''
-      choose the colorscheme of LazyVim
-    '';
-  };
-  config = {
-    my.neovim.lazyvim.extraPlugins = with pkgs.vimPlugins; [
-      tokyonight-nvim
-      kanagawa-nvim
-      catppuccin-nvim
-    ];
+  # options.my.neovim.lazyvim.colorscheme = mkOption {
+  #   type = enum ["tokyonight" "kanagawa" "catppuccin"];
+  #   default = "tokyonight";
+  #   description = ''
+  #     choose the colorscheme of LazyVim
+  #   '';
+  # };
 
+  config = {
+    my.neovim.lazyvim.extraPlugins = with pkgs.vimPlugins;
+      if theme == "tokyonight"
+      then [
+        tokyonight-nvim
+      ]
+      else if theme == "kanagawa"
+      then [
+        kanagawa-nvim
+      ]
+      else if theme == "catppuccin"
+      then [
+        catppuccin-nvim
+      ]
+      else [];
+
+    # FIXME: not working when theme is auto
     xdg.configFile = mkMerge [
-      (sourceLua "colorscheme/${cfg}.lua")
+      (sourceLua "colorscheme/${theme}.lua")
     ];
   };
 }
