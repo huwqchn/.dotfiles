33d8392c87c9a13a220d0697aa90e424af10c05b
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-18 16:47:28 +0800

    fix: systemd

--- modules/nixos/networking/systemd.nix
+++ modules/nixos/networking/systemd.nix
@@ -9,33 +9,35 @@
   inherit (lib.attrsets) concatMapAttrs genAttrs;
   inherit (config.my.machine) ethernetDevices;
 in {
-  options.my.machine.networkDevices = mkOption {
+  options.my.machine.ethernetDevices = mkOption {
     type = listOf str;
     default = [];
     description = "The network devices of the system";
   };
 
-  # systemd DNS resolver daemon
-  services.resolved.enable = true;
+  config = {
+    # systemd DNS resolver daemon
+    services.resolved.enable = true;
 
-  systemd = {
-    # allow for the system to boot without waiting for the network interfaces are online
-    network.wait-online.enable = false;
+    systemd = {
+      # allow for the system to boot without waiting for the network interfaces are online
+      network.wait-online.enable = false;
 
-    services =
-      {
-        # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
-        NetworkManager-wait-online.enable = false;
+      services =
+        {
+          # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
+          NetworkManager-wait-online.enable = false;
 
-        # disable networkd and resolved from being restarted on configuration changes
-        # also prevents failures from services that are restarted instead of stopped
-        systemd-networkd.stopIfChanged = false;
-        systemd-resolved.stopIfChanged = false;
-      }
-      // concatMapAttrs (_: v: v) (
-        genAttrs ethernetDevices (device: {
-          "network-addresses-${device}".wantedBy = mkForce ["sys-subsystem-net-devices-${device}.device"];
-        })
-      );
+          # disable networkd and resolved from being restarted on configuration changes
+          # also prevents failures from services that are restarted instead of stopped
+          systemd-networkd.stopIfChanged = false;
+          systemd-resolved.stopIfChanged = false;
+        }
+        // concatMapAttrs (_: v: v) (
+          genAttrs ethernetDevices (device: {
+            "network-addresses-${device}".wantedBy = mkForce ["sys-subsystem-net-devices-${device}.device"];
+          })
+        );
+    };
   };
 }
