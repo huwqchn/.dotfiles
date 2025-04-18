44415cf046ca1c7329474b0aad03c23e17dab64b
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-18 15:57:35 +0800

    fix: clamav

--- modules/nixos/security/clamav.nix
+++ modules/nixos/security/clamav.nix
@@ -1,125 +1,183 @@
 {
-  config,
-  pkgs,
   lib,
+  pkgs,
+  config,
   ...
 }: let
-  inherit (lib.attrsets) filterAttrs mapAttrsToList;
-  inherit (lib.options) mkEnableOption;
-  inherit (lib.modules) mkIf;
+  inherit (lib.modules) mkIf mkForce;
+  inherit (lib.options) mkOption mkEnableOption;
+  inherit (lib.strings) escapeShellArgs;
+  inherit
+    (lib.types)
+    int
+    str
+    bool
+    oneOf
+    listOf
+    attrsOf
+    ;
 
   cfg = config.my.security.clamav;
-
-  sus-user-dirs = ["Downloads"];
-  all-normal-users = filterAttrs (_username: config: config.isNormalUser) config.users.users;
-  all-sus-dirs =
-    builtins.concatMap (
-      dir: mapAttrsToList (_username: config: config.home + "/" + dir) all-normal-users
-    )
-    sus-user-dirs;
-  # all-user-folders = attrsets.mapAttrsToList (_username: config: config.home) all-normal-users;
-  all-system-folders = [
-    "/boot"
-    "/etc"
-    "/nix"
-    "/root"
-    "/usr"
-  ];
-  notify-all-users = pkgs.writeScript "notify-all-users-of-sus-file" ''
-    #!/usr/bin/env bash
-    ALERT="Signature detected by clamav: $CLAM_VIRUSEVENT_VIRUSNAME in $CLAM_VIRUSEVENT_FILENAME"
-    # Send an alert to all graphical users.
-    for ADDRESS in /run/user/*; do
-        USERID=''${ADDRESS#/run/user/}
-       /run/wrappers/bin/sudo -u "#$USERID" DBUS_SESSION_BUS_ADDRESS="unix:path=$ADDRESS/bus" ${pkgs.notify-desktop}/bin/notify-desktop -i dialog-warning "Sus file" "$ALERT"
-    done
-  '';
 in {
   options.my.security.clamav = {
     enable =
-      mkEnableOption "Enable ClamAV antivirus";
-    # // {
-    #   default = config.my.security.enable;
-    # };
+      mkEnableOption "Enable ClamAV daemon."
+      // {
+        default = config.my.security.enable;
+      };
+
+    daemon = {
+      settings = mkOption {
+        type = attrsOf (oneOf [
+          bool
+          int
+          str
+          (listOf str)
+        ]);
+        default = {
+          LogFile = "/var/log/clamd.log";
+          LogTime = true;
+          DetectPUA = true;
+          VirusEvent = escapeShellArgs [
+            "${pkgs.libnotify}/bin/notify-send"
+            "--"
+            "ClamAV Virus Scan"
+            "Found virus: %v"
+          ];
+        };
+
+        description = ''
+          ClamAV configuration. Refer to <https://linux.die.net/man/5/clamd.conf>,
+          for details on supported values.
+        '';
+      };
+    };
+
+    updater = {
+      enable = mkEnableOption "ClamAV freshclam updater";
+
+      frequency = mkOption {
+        type = int;
+        default = 12;
+        description = ''
+          Number of database checks per day.
+        '';
+      };
+
+      interval = mkOption {
+        type = str;
+        default = "hourly";
+        description = ''
+          How often freshclam is invoked. See systemd.time(7) for more
+          information about the format.
+        '';
+      };
+
+      settings = mkOption {
+        type = attrsOf (oneOf [
+          bool
+          int
+          str
+          (listOf str)
+        ]);
+        default = {};
+        description = ''
+          freshclam configuration. Refer to <https://linux.die.net/man/5/freshclam.conf>,
+          for details on supported values.
+        '';
+      };
+    };
   };
 
   config = mkIf cfg.enable {
-    security.sudo = {
-      extraConfig = ''
-        clamav ALL = (ALL) NOPASSWD: SETENV: ${pkgs.notify-desktop}/bin/notify-desktop
-      '';
+    services.clamav = {
+      daemon =
+        {
+          enable = true;
+        }
+        // cfg.daemon;
+      updater =
+        {
+          enable = true;
+        }
+        // cfg.updater;
     };
 
-    services.clamav = {
-      daemon = {
-        enable = true;
-        settings = {
-          ConcurrentDatabaseReload = false;
-          OnAccessIncludePath = all-sus-dirs;
-          OnAccessPrevention = false;
-          OnAccessExtraScanning = true;
-          OnAccessExcludeUname = "clamav";
-          VirusEvent = "${notify-all-users}";
-          User = "clamav";
+    systemd = {
+      tmpfiles.settings."10-clamav"."/var/lib/clamav".D = {
+        mode = "755";
+        user = "clamav";
+        group = "clamav";
+      };
+
+      services = {
+        clamav-daemon = {
+          serviceConfig = {
+            PrivateTmp = mkForce "no";
+            PrivateNetwork = mkForce "no";
+            Restart = "always";
+          };
+
+          unitConfig = {
+            # only start clamav when required database files are present
+            # especially useful if you are deploying headlessly and don't want a service fail instantly
+            ConditionPathExistsGlob = [
+              "/var/lib/clamav/main.{c[vl]d,inc}"
+              "/var/lib/clamav/daily.{c[vl]d,inc}"
+            ];
+          };
+        };
+
+        clamav-init-database = {
+          wantedBy = ["clamav-daemon.service"];
+          before = ["clamav-daemon.service"];
+          serviceConfig.ExecStart = "systemctl start clamav-freshclam";
+          unitConfig = {
+            # opposite condition of clamav-daemon: only run this service if
+            # database files are not present in the database directory
+            ConditionPathExistsGlob = [
+              "!/var/lib/clamav/main.{c[vl]d,inc}"
+              "!/var/lib/clamav/daily.{c[vl]d,inc}"
+            ];
+          };
+        };
+
+        clamav-freshclam = {
+          wants = ["clamav-daemon.service"];
+          serviceConfig = {
+            ExecStart = let
+              message = "Updating ClamAV database";
+            in ''
+              ${pkgs.coreutils}/bin/echo -en ${message}
+            '';
+            SuccessExitStatus = mkForce [
+              11
+              40
+              50
+              51
+              52
+              53
+              54
+              55
+              56
+              57
+              58
+              59
+              60
+              61
+              62
+            ];
+          };
         };
       };
-      updater = {
-        enable = true;
-        interval = "daily";
-        frequency = 2;
-      };
-    };
 
-    systemd.services.clamav-clamonacc = {
-      description = "ClamAV daemon (clamonacc)";
-      after = ["clamav-freshclam.service"];
-      wantedBy = ["multi-user.target"];
-      restartTriggers = ["/etc/clamav/clamd.conf"];
-
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.systemd}/bin/systemd-cat --identifier=av-scan ${pkgs.clamav}/bin/clamonacc -F --fdpass";
-        PrivateTmp = "yes";
-        PrivateDevices = "yes";
-        PrivateNetwork = "yes";
-      };
-    };
-
-    #systemd.timers.av-user-scan = {
-    #  description = "scan normal user directories for suspect files";
-    #  wantedBy = [ "timers.target" ];
-    #  timerConfig = {
-    #    OnCalendar = "weekly";
-    #    Unit = "av-user-scan.service";
-    #  };
-    #};
-
-    #systemd.services.av-user-scan = {
-    #  description = "scan normal user directories for suspect files";
-    #  after = [ "multi-user.target" ];
-    #  serviceConfig = {
-    #    Type = "oneshot";
-    #    ExecStart = "${pkgs.systemd}/bin/systemd-cat --identifier=av-scan ${pkgs.clamav}/bin/clamdscan --quiet --recursive --fdpass ${toString all-user-folders}";
-    #  };
-    #};
-
-    systemd.timers.av-all-scan = {
-      description = "scan all directories for suspect files";
-      wantedBy = ["timers.target"];
-      timerConfig = {
-        OnCalendar = "monthly";
-        Unit = "av-all-scan.service";
-      };
-    };
-
-    systemd.services.av-all-scan = {
-      description = "scan all directories for suspect files";
-      after = ["multi-user.target"];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ''
-          ${pkgs.systemd}/bin/systemd-cat --identifier=av-scan ${pkgs.clamav}/bin/clamdscan --quiet --recursive --fdpass ${toString all-system-folders}
-        '';
+      timers.clamav-freshclam.timerConfig = {
+        # the default is to run the timer hourly but we do not want our entire infra to be overloaded
+        # trying to run clamscan at the same time. randomize the timer to something around an hour
+        # so that the window is consistent, but the load is not
+        RandomizedDelaySec = "60m";
+        FixedRandomDelay = true;
+        Persistent = true;
       };
     };
   };
