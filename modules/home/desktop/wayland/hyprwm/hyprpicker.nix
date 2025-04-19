97816cbad6dddf5d424a4426e4de504316c853a5
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-19 20:19:42 +0800

    feat: add hyprpicker --skip-check

---
+++ modules/home/desktop/wayland/hyprwm/hyprpicker.nix
@@ -0,0 +1,21 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  cfg = config.my.desktop.hyprland;
+in {
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      hyprpicker
+      (writeShellScriptBin "hyprpicker-wl-copy" ''
+        ${hyprpicker}/bin/hyprpicker \
+        | tee >(${wl-clipboard}/bin/wl-copy) \
+        >(${libnotify}/bin/notify-send "Color picked" "$(cat)") \
+        >/dev/null
+      '')
+    ];
+  };
+}
