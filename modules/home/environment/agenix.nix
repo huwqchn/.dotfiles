dd06c0f2e4d34c18c2303080db2f83dac209453a
Johnson Hu <johnson.wq.hu@gmail.com>
2025-04-24 01:55:32 +0800

    fix: fuse --skip-check

--- modules/home/environment/agenix.nix
+++ modules/home/environment/agenix.nix
@@ -5,6 +5,7 @@
   pkgs,
   ...
 }: let
+  inherit (lib.modules) mkIf;
   inherit (config.home) homeDirectory;
   isLinuxSystemd = pkgs.stdenv.hostPlatform.isLinux && config.systemd.user.services ? agenix;
 in {
@@ -31,12 +32,8 @@ in {
 
     # You must set this, if you want to use agenix home manager module on nixos
     # see: https://github.com/ryantm/agenix/issues/50#issuecomment-1926893522
-    activation.agenix = lib.hm.dag.entryAfter ["writeBoundary"] (
-      if isLinuxSystemd
-      then ''
-        ${config.systemd.user.services.agenix.Service.ExecStart}
-      ''
-      else ""
-    );
+    activation = mkIf isLinuxSystemd {
+      agenix = lib.hm.dag.entryAfter ["writeBoundary"] config.systemd.user.services.agenix.Service.ExecStart;
+    };
   };
 }
--- modules/home/environment/impermanence.nix
+++ modules/home/environment/impermanence.nix
@@ -2,48 +2,50 @@
   inputs,
   lib,
   config,
+  pkgs,
   ...
 }: let
-  persist = config.my.persistence.enable;
   inherit (lib.modules) mkForce mkIf;
-  cleanupDeadFUSE = {
-    name = "cleanup-dead-fuse";
-    text = ''
-      for mp in $(findmnt -t fuse.* -n -o TARGET); do
-        fusermount -uz "$mp" || true
-      done
-    '';
-  };
+  inherit (lib.meta) getExe';
+  persist = config.my.persistence.enable;
+  fusermount' = getExe' pkgs.fuse "fusermount";
+  findmnt' = lib.getExe' pkgs.util-linux "findmnt";
 in {
   imports = [
     inputs.impermanence.homeManagerModules.impermanence
   ];
 
-  home.persistence =
-    if persist
-    then {
-      "/persist${config.home.homeDirectory}" = {
-        directories = [
-          ".local/bin"
-          ".cache/nix"
-          ".cache/pre-commit"
-          ".dotfiles"
-          ".docker"
-          ".secrets"
-          "Documents"
-          "Downloads"
-          "Desktop"
-          "Media"
-          "Public"
-          "Dev"
-          "Misc"
-        ];
-        allowOther = true;
-      };
-    }
-    else mkForce {};
+  home = {
+    persistence =
+      if persist
+      then {
+        "/persist${config.home.homeDirectory}" = {
+          directories = [
+            ".local/bin"
+            ".cache/nix"
+            ".cache/pre-commit"
+            ".dotfiles"
+            ".docker"
+            ".secrets"
+            "Documents"
+            "Downloads"
+            "Desktop"
+            "Media"
+            "Public"
+            "Dev"
+            "Misc"
+          ];
+          allowOther = true;
+        };
+      }
+      else mkForce {};
 
-  home.activation = mkIf persist (
-    lib.hm.dag.before ["persistence-init"] cleanupDeadFUSE
-  );
+    activation = mkIf persist {
+      cleanup-dead-fuse = lib.hm.dag.entryBefore ["persistence-init"] ''
+        for mp in $(${findmnt'} -t fuse.* -n -o TARGET); do
+          ${fusermount'} -uz "$mp" || true
+        done
+      '';
+    };
+  };
 }
