1f170a84c18569853973d90fa0fac37022fc4e04
Johnson Hu <johnson.wq.hu@gmail.com>
2025-08-07 06:21:44 +0800

    chore: enable useUserPackages

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -43,8 +43,6 @@ in {
     inherit extraSpecialArgs;
     backupFileExtension = "backup";
     useGlobalPkgs = true;
-    # do not enable home-manager.useUserPackages, to match standalone home-manager,
-    # so home-manager/nixos-rebuild/darwin-rebuild can be used at the same time
-    # home-manager.useUserPackages = true;
+    useUserPackages = true;
   };
 }
