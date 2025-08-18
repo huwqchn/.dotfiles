b490e9ec7ceb864c027d0b5fcbc08017ee062e74
Johnson Hu <johnson.wq.hu@gmail.com>
2025-08-19 07:12:55 +0800

    chore: update

--- modules/common/secrets.nix
+++ modules/common/secrets.nix
@@ -1,13 +1,16 @@
 {
   config,
   self,
+  lib,
   ...
 }: let
+  inherit (lib.strings) optionalString;
   inherit (config.my) home;
+  persist = config.my.persistence.enable;
 in {
   sops = {
     defaultSopsFile = "${self}/secrets/default.yaml";
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    age.sshKeyPaths = ["${optionalString persist "/persist"}/etc/ssh/ssh_host_ed25519_key"];
     gnupg.home = "${home}/.gnupg";
   };
 }
