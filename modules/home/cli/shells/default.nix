5d9310bc9d4c7074e3d39454bd85d93930425f7a
Johnson Hu <johnson.wq.hu@gmail.com>
2025-08-19 02:28:50 +0800

    feat: add ai coder

--- modules/home/cli/shells/default.nix
+++ modules/home/cli/shells/default.nix
@@ -1,1 +1,18 @@
-{lib, ...}: {imports = lib.my.scanPaths ./.;}
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.meta) getExe' getExe;
+  bunx' = getExe' pkgs.bun "bunx";
+  curl' = getExe pkgs.curl;
+in {
+  imports = lib.my.scanPaths ./.;
+  home.shellAliases = {
+    weather = "${curl'} wttr.in";
+    gemini = "${bunx'} @google/gemini-cli";
+    claude = "${bunx'} @anthropic-ai/claude-code";
+    codex = "${bunx'} @openai/codex";
+    opencode = "${bunx'} opencode-ai";
+  };
+}
