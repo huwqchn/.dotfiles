73f91605f5bccd73dee0b793cac2510e1460e919
Johnson Hu <johnson.wq.hu@gmail.com>
2025-08-27 17:07:56 +0800

    feat(home): add Zotero app module
    Defines option my.desktop.apps.zotero.enable (default: my.desktop.enable). Installs zotero, tesseract, and poppler_utils when enabled.

---
+++ modules/home/desktop/apps/zotero.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+  cfg = config.my.desktop.apps.zotero;
+in {
+  options.my.desktop.apps.zotero = {
+    enable =
+      mkEnableOption "Zotero"
+      // {
+        default = config.my.desktop.enable;
+      };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      zotero
+      tesseract
+      poppler_utils
+    ];
+  };
+}
